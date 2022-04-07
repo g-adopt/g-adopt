44cfee6189f9c78785b246eb1960321b3bf59b64
Rhodri Davies <rhodri.davies@anu.edu.au>
2022-04-07 17:05:42 +1000

    Start of spherical delta function case.

---
+++ Davies_etal_GMD_2021/analytical_comparisons/delta/spherical/free_slip/template/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+TEMPLATENAME
+#PBS -P xd2
+TEMPLATEQUEUE
+#PBS -l walltime=05:00:00 
+TEMPLATEMEM
+TEMPLATECPUS
+#PBS -l jobfs=10GB
+#PBS -l storage=scratch/xd2+gdata/xd2
+#PBS -l wd
+#### Load relevant modules:
+module use /g/data/xd2/modulefiles
+module load firedrake/firedrake-20220301
+export OMP_NUM_THREADS=1
+#### Now run:
+mpirun -np $PBS_NCPUS python stokes_trilinear.py LTEMPLATE MTEMPLATE TEMPLATELEVELS TEMPLATELAYERS &> output.dat
