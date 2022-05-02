17e66c290b6efaf1cb75f03f59cf8dda86e6e294
Rhodri Davies <rad552@gadi-login-06.gadi.nci.org.au>
2022-05-02 10:08:07 +1000

    Update parallel scaling post-processing scripts.

--- Davies_etal_GMD_2021/parallel_scaling/plot_scaling_diagnostics.py
+++ Davies_etal_GMD_2021/parallel_scaling/plot_scaling_diagnostics.py
@@ -8,16 +8,13 @@ from matplotlib import rcParams
 preamble = sys.argv[1]
 
 # Matplotlib defaults:
-rcParams['axes.titlesize'] = 15
-rcParams['axes.labelsize'] = 15
-rcParams['xtick.labelsize'] = 15
-rcParams['ytick.labelsize'] = 15
+rcParams['axes.titlesize'] = 16
+rcParams['axes.labelsize'] = 16
+rcParams['xtick.labelsize'] = 16
+rcParams['ytick.labelsize'] = 16
 rcParams['legend.fontsize'] = 15
 
-# First, gather data
-
-# dirs  = ["Level_4", "Level_5","Level_6", "Level_7"]
-# cores = [24, 192, 1536, 12288]
+# FIRST GATHER DATA
 
 dirs = ["Level_5", "Level_6", "Level_7", "Level_8"]
 cores = [24, 192, 1536, 12288]
@@ -32,6 +29,8 @@ PC_setup_time = numpy.zeros((len(dirs), 1))
 mean_velocity_its = numpy.zeros((len(dirs), 1))
 mean_pressure_its = numpy.zeros((len(dirs), 1))
 mean_energy_its = numpy.zeros((len(dirs), 1))
+velocity_cost_per_it = numpy.zeros((len(dirs), 1))
+total_cost = numpy.zeros((len(dirs), 1))
 
 # Loop over directiories and store data:
 for dir in range(len(dirs)):
@@ -44,25 +43,28 @@ for dir in range(len(dirs)):
     mean_velocity_its[dir, :] = data[5]
     mean_pressure_its[dir, :] = data[6]
     mean_energy_its[dir, :] = data[7]
+    total_cost[dir, :] = data[8]
 
 assembly_time = SNES_function_time + SNES_jacobian_time
+velocity_cost_per_it = (schur_solve_time - PC_setup_time) / mean_velocity_its
 
-# Now plot:
+# NOW PLOT
+
 # Schur solve time
-pylab.plot(cores, schur_solve_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, schur_solve_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("Stokes Solve (s)")
-pylab.xlim(-500, 12500)
+pylab.xlim(2, 20000)
 pylab.xticks(numpy.linspace(0, 12500, 6))
-pylab.ylim(7000, 13000)
-pylab.yticks(numpy.linspace(7000, 13000, 4))
+pylab.ylim(6000, 14000)
+pylab.yticks(numpy.linspace(6000, 14000, 5))
 pylab.grid()
 pylab.savefig(preamble+"_Schur_Solve_time.png", bbox_inches="tight")
 pylab.savefig(preamble+"_Schur_Solve_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # Energy solve time
-pylab.plot(cores, energy_solve_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, energy_solve_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("Energy Solve (s)")
 pylab.xlim(-500, 12500)
@@ -75,7 +77,7 @@ pylab.savefig(preamble+"_Energy_Solve_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # SNES Function time
-pylab.plot(cores, SNES_function_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, SNES_function_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("SNES Function (s)")
 pylab.xlim(-500, 12500)
@@ -86,7 +88,7 @@ pylab.savefig(preamble+"_SNES_function_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # SNES Jacobian time
-pylab.plot(cores, SNES_jacobian_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, SNES_jacobian_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("SNES Jacobian (s)")
 pylab.xlim(-500, 12500)
@@ -97,20 +99,20 @@ pylab.savefig(preamble+"_SNES_jacobian_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # Assembly time
-pylab.plot(cores, assembly_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, assembly_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("Assembly (s)")
 pylab.xlim(-500, 12500)
 pylab.xticks(numpy.linspace(0, 12500, 6))
-pylab.ylim(4, 10)
-pylab.yticks(numpy.linspace(4, 10, 4))
+pylab.ylim(0, 5)
+pylab.yticks(numpy.linspace(0, 5, 3))
 pylab.grid()
 pylab.savefig(preamble+"_assembly_time.png", bbox_inches="tight")
 pylab.savefig(preamble+"_assembly_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # PC Setup Time
-pylab.plot(cores, PC_setup_time, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, PC_setup_time, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("PC Setup (s)")
 pylab.xlim(-500, 12500)
@@ -123,7 +125,7 @@ pylab.savefig(preamble+"_PC_Setup_time.pdf", bbox_inches="tight")
 pylab.close()
 
 # Pressure iterations
-pylab.plot(cores, mean_pressure_its, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, mean_pressure_its, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("# Pressure Solve Iterations")
 pylab.xlim(-500, 12500)
@@ -135,8 +137,8 @@ pylab.savefig(preamble+"_Pressure_Iterations.png", bbox_inches="tight")
 pylab.savefig(preamble+"_Pressure_Iterations.pdf", bbox_inches="tight")
 pylab.close()
 
-# Velocity iterations
-pylab.plot(cores, mean_velocity_its, 'ro', linestyle="none", markersize=8)
+# Velocity iterations ###
+pylab.plot(cores, mean_velocity_its, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("# Velocity Solve Iterations")
 pylab.xlim(-500, 12500)
@@ -149,7 +151,7 @@ pylab.savefig(preamble+"_Velocity_Iterations.pdf", bbox_inches="tight")
 pylab.close()
 
 # Energy iterations
-pylab.plot(cores, mean_energy_its, 'ro', linestyle="none", markersize=8)
+pylab.plot(cores, mean_energy_its, 'ko', linestyle="none", markersize=10)
 pylab.xlabel("# Cores")
 pylab.ylabel("# Energy Solve Iterations")
 pylab.xlim(-500, 12500)
@@ -161,37 +163,46 @@ pylab.savefig(preamble+"_Energy_Iterations.png", bbox_inches="tight")
 pylab.savefig(preamble+"_Energy_Iterations.pdf", bbox_inches="tight")
 pylab.close()
 
+# Velocity cost per iteration
+pylab.plot(cores, velocity_cost_per_it, 'ko', linestyle="none", markersize=10)
+pylab.xlabel("# Cores")
+pylab.ylabel("Per Velocity Iteration (s)")
+pylab.xlim(-500, 12500)
+pylab.xticks(numpy.linspace(0, 12500, 6))
+pylab.ylim(150, 230)
+pylab.yticks(numpy.linspace(150, 230, 5))
+pylab.grid()
+pylab.savefig(preamble+"_Velocity_Cost_Per_Iteration.png", bbox_inches="tight")
+pylab.savefig(preamble+"_Velocity_Cost_Per_Iteration.pdf", bbox_inches="tight")
+pylab.close()
+
+# Total Time
+pylab.plot(cores, total_cost, 'ko', linestyle="none", markersize=10)
+pylab.xlabel("# Cores")
+pylab.ylabel("Total Time (s)")
+pylab.xlim(-500, 12500)
+pylab.xticks(numpy.linspace(0, 12500, 6))
+pylab.ylim(6000, 14000)
+pylab.yticks(numpy.linspace(6000, 14000, 5))
+pylab.grid()
+pylab.savefig(preamble+"_Total_Time.png", bbox_inches="tight")
+pylab.savefig(preamble+"_Total_Time.pdf", bbox_inches="tight")
+pylab.close()
+
 # Make some combination plots:
+
 # Iterations
-pylab.plot(cores, mean_energy_its, 'mo', linestyle="none", markersize=8, label="Energy")
-pylab.plot(cores, mean_velocity_its, 'g*', linestyle="none", markersize=8, label="Velocity")
-pylab.plot(cores, mean_pressure_its, 'bs', linestyle="none", markersize=8, label="Pressure")
+pylab.plot(cores, mean_velocity_its, 'go', linestyle="none", markersize=10, label="Velocity")
+pylab.plot(cores, mean_pressure_its, 'rs', linestyle="none", markersize=10, label="Pressure")
+pylab.plot(cores, mean_energy_its, 'b*', linestyle="none", markersize=10, label="Energy")
 pylab.xlabel("# Cores")
 pylab.ylabel("# Solve Iterations")
 pylab.xlim(-500, 12500)
 pylab.xticks(numpy.linspace(0, 12500, 6))
-pylab.ylim(0, 60)
-pylab.yticks(numpy.linspace(0, 60, 4))
+pylab.ylim(5, 75)
+pylab.yticks(numpy.linspace(10, 70, 4))
 pylab.grid()
 pylab.legend(numpoints=1)
 pylab.savefig(preamble+"_Combined_Iterations.png", bbox_inches="tight")
 pylab.savefig(preamble+"_Combined_Iterations.pdf", bbox_inches="tight")
 pylab.close()
-
-
-# Energy
-# fig, ax1 = pylab.subplots()
-# color = 'tab:red'
-# ax1.set_xlabel('# Cores (s)')
-# ax1.set_ylabel('# Energy Solve Iterations', color=color)
-# ax1.plot(cores, mean_energy_its, 'o', color=color, linestyle="none", markersize=8)
-# ax1.tick_params(axis='y', labelcolor=color)
-# ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-# color = 'tab:blue'
-# ax2.set_ylabel('Energy Solve Time (s)', color=color)  # we already handled the x-label with ax1
-# ax2.plot(cores, energy_solve_time, 'o', color=color, linestyle="none", markersize=8)
-# ax2.tick_params(axis='y', labelcolor=color)
-# fig.tight_layout()  # otherwise the right y-label is slightly clipped
-# pylab.savefig(preamble+"_Combined_Energy.png",bbox_inches="tight")
-# pylab.savefig(preamble+"_Combined_Energy.pdf",bbox_inches="tight")
-# pylab.close()
