fba41a17eeefc0755d7d181f57c9deb1c1701a02
Angus Gibson <angus@agibson.me>
2022-05-10 16:01:30 +1000

    Add a tested version of the minimal base case

--- Davies_etal_GMD_2021/test_all.py
+++ Davies_etal_GMD_2021/test_all.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 cases = [
     "2D_cartesian/base_case",
+    ("2D_cartesian/base_case", "minimal_"),
     "2D_cartesian/compressible_case",
     "2D_cartesian/viscoplastic_case",
     "2D_cylindrical",
@@ -12,14 +13,19 @@ cases = [
 ]
 
 
-def get_convergence(base):
-    return pd.read_csv(base / "params.log", sep="\\s+", header=0).iloc[-1]
+def get_convergence(base, prefix=""):
+    return pd.read_csv(base / f"{prefix}params.log", sep="\\s+", header=0).iloc[-1]
 
 
 @pytest.mark.parametrize("benchmark", cases)
 def test_benchmark(benchmark):
+    # multiple cases in one directory, give a prefix to the files
+    prefix = ""
+    if isinstance(benchmark, tuple):
+        benchmark, prefix = benchmark
+
     b = Path(__file__).parent.resolve() / benchmark
-    df = get_convergence(b)
-    expected = pd.read_pickle(b / "expected.pkl")
+    df = get_convergence(b, prefix)
+    expected = pd.read_pickle(b / f"{prefix}expected.pkl")
 
     pd.testing.assert_series_equal(df[["u_rms", "nu_top"]], expected)
