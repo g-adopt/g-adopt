98f4bb53b730386d03e153b818e29363d687549a
Rhodri Davies <rhodri.davies@anu.edu.au>
2022-10-27 12:06:35 +1100

    Start of 3D cartesian demo. Currently failing the energy solve when calculating normals. Also will need to update geodynamical diagnostics

---
+++ demos/3d_cartesian/3d_cartesian.py
@@ -0,0 +1,147 @@
+from gadopt import *
+from mpi4py import MPI
+
+# Set up geometry:
+a, b, c = 1.0079, 0.6283, 1.0
+nx, ny, nz = 20, int(b/c * 20), 20
+mesh2d = RectangleMesh(nx, ny, a, b, quadrilateral=True)  # Rectangular 2D mesh
+mesh = ExtrudedMesh(mesh2d, nz)
+bottom_id, top_id, left_id, right_id, front_id, back_id = "bottom", "top", 1, 2, 3, 4
+
+# Set up function spaces - currently using the bilinear Q2Q1 element pair:
+V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+Z = MixedFunctionSpace([V, W])  # Mixed function space.
+
+# Function to store the solutions:
+z = Function(Z)  # a field over the mixed function space Z.
+u, p = split(z)  # Returns symbolic UFL expression for u and p
+
+# Output function space information:
+log("Number of Velocity DOF:", V.dim())
+log("Number of Pressure DOF:", W.dim())
+log("Number of Velocity and Pressure DOF:", V.dim()+W.dim())
+log("Number of Temperature DOF:", Q.dim())
+
+# Set up temperature field and initialise:
+X = SpatialCoordinate(mesh)
+T = Function(Q, name="Temperature")
+T.interpolate(0.5*(erf((1-X[2])*4)+erf(-X[2]*4)+1) + 0.2*(cos(pi*X[0]/a)+cos(pi*X[1]/b))*sin(pi*X[2]))
+
+delta_t = Constant(1e-6)  # Initial time-step
+t_adapt = TimestepAdaptor(delta_t, V, maximum_timestep=0.1, increase_tolerance=1.5)
+
+# Stokes related constants (note that since these are included in UFL, they are wrapped inside Constant):
+Ra = Constant(3e4)  # Rayleigh number
+approximation = BoussinesqApproximation(Ra)
+
+time = 0.0
+steady_state_tolerance = 1e-7 # Set to 1e-9 for simulations in GMD paper.
+max_timesteps = 50000
+kappa = Constant(1.0)  # Thermal diffusivity
+
+# Nullspaces and near-nullspaces:
+Z_nullspace = create_stokes_nullspace(Z, closed=True, rotational=False)
+Z_near_nullspace = create_stokes_nullspace(Z, closed=False, rotational=True, translations=[0,1])
+
+# Write output files in VTK format:
+u, p = z.split()  # Do this first to extract individual velocity and pressure fields.
+# Next rename for output:
+u.rename("Velocity")
+p.rename("Pressure")
+# Create output file and select output_frequency:
+output_file = File("output.pvd")
+dump_period = 50
+# Frequency of checkpoint files:
+checkpoint_period = dump_period * 4
+
+# Open file for logging diagnostic output:
+plog = ParameterLog('params.log', mesh)
+
+#gd = GeodynamicalDiagnostics(u, p, T, bottom_id, top_id)
+
+
+temp_bcs = {
+    bottom_id: {'T': 1.0},
+    top_id: {'T': 0.0},
+}
+
+stokes_bcs = {
+    bottom_id: {'ux': 0},
+    bottom_id: {'uy': 0},    
+    top_id: {'ux': 0},
+    top_id: {'uy': 0},    
+    left_id: {'ux': 0},
+    right_id: {'ux': 0},
+    front_id: {'uy': 0},
+    back_id: {'uy': 0},    
+}
+
+energy_solver = EnergySolver(T, u, approximation, delta_t, ImplicitMidpoint, bcs=temp_bcs)
+Told = energy_solver.T_old
+Ttheta = 0.5*T + 0.5*Told
+Told.assign(T)
+stokes_solver = StokesSolver(z, Ttheta, approximation, delta_t, bcs=stokes_bcs,
+                             cartesian=True,
+                             nullspace=Z_nullspace, transpose_nullspace=Z_nullspace,
+                             near_nullspace=Z_near_nullspace)
+
+# Now perform the time loop:
+for timestep in range(0, max_timesteps):
+
+    # Write output:
+    if timestep % dump_period == 0:
+        output_file.write(u, p, T)
+
+    dt = t_adapt.update_timestep(u)
+    time += dt
+
+    # Solve Stokes sytem:
+    stokes_solver.solve()
+
+    # Temperature system:
+    energy_solver.solve()
+
+    # Compute diagnostics:
+    bcu = DirichletBC(u.function_space(), 0, top_id)
+    ux_max = u.dat.data_ro_with_halos[bcu.nodes, 0].max(initial=0)
+    ux_max = u.comm.allreduce(ux_max, MPI.MAX)  # Maximum Vx at surface
+#    nusselt_number_top = gd.Nu_top()
+#    nusselt_number_base = gd.Nu_bottom()
+    energy_conservation = abs(abs(nusselt_number_top) - abs(nusselt_number_base))
+
+    # Calculate L2-norm of change in temperature:
+    maxchange = sqrt(assemble((T - energy_solver.T_old)**2 * dx))
+
+    # Log diagnostics:
+    plog.log_str(f"{timestep} {time} {float(delta_t)} {maxchange} {gd.u_rms()} {gd.u_rms_top()} {ux_max} "
+                 f"{nusselt_number_top} {nusselt_number_base} "
+                 f"{energy_conservation} {gd.T_avg()} ")
+
+    # Leave if steady-state has been achieved:
+    if maxchange < steady_state_tolerance:
+        log("Steady-state achieved -- exiting time-step loop")
+        break
+
+    # Checkpointing:
+    if timestep % checkpoint_period == 0:
+        # Checkpointing during simulation:
+        checkpoint_data = DumbCheckpoint(f"Temperature_State_{timestep}", mode=FILE_CREATE)
+        checkpoint_data.store(T, name="Temperature")
+        checkpoint_data.close()
+
+        checkpoint_data = DumbCheckpoint(f"Stokes_State_{timestep}", mode=FILE_CREATE)
+        checkpoint_data.store(z, name="Stokes")
+        checkpoint_data.close()
+
+plog.close()
+
+# Write final state:
+final_checkpoint_data = DumbCheckpoint("Final_Temperature_State", mode=FILE_CREATE)
+final_checkpoint_data.store(T, name="Temperature")
+final_checkpoint_data.close()
+
+final_checkpoint_data = DumbCheckpoint("Final_Stokes_State", mode=FILE_CREATE)
+final_checkpoint_data.store(z, name="Stokes")
+final_checkpoint_data.close()
