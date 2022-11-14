733fcb3d25ce2bf2108e08ae901e06d29b5e81ce
Rhodri Davies <rhodri.davies@anu.edu.au>
2022-11-15 10:46:25 +1100

    Start of 3D spherical demo.

---
+++ demos/3d_spherical/3d_spherical.py
@@ -0,0 +1,183 @@
+from gadopt import *
+import scipy.special
+import math
+
+# Quadrature degree:
+dx = dx(degree=6)
+
+# Set up geometry:
+rmin, rmax, ref_level, nlayers = 1.22, 2.22, 4, 8
+
+# Construct a CubedSphere mesh and then extrude into a sphere:
+mesh2d = CubedSphereMesh(rmin, refinement_level=ref_level, degree=2)
+mesh = ExtrudedMesh(mesh2d, layers=nlayers, extrusion_type='radial')
+bottom_id, top_id = "bottom", "top"
+n = FacetNormal(mesh)  # Normals, required for Nusselt number calculation
+domain_volume = assemble(1*dx(domain=mesh))  # Required for diagnostics (e.g. RMS velocity)
+
+
+# Set up function spaces - currently using the bilinear Q2Q1 element pair:
+V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+Z = MixedFunctionSpace([V, W])  # Mixed function space.
+Qlayer = FunctionSpace(mesh2d, "CG", 2)  # used to compute layer average
+
+# Test functions and functions to hold solutions:
+z = Function(Z)  # a field over the mixed function space Z.
+u, p = split(z)  # Returns symbolic UFL expression for u and p
+
+# Output function space information:
+log("Number of Velocity DOF:", V.dim())
+log("Number of Pressure DOF:", W.dim())
+log("Number of Velocity and Pressure DOF:", V.dim() + W.dim())
+log("Number of Temperature DOF:", Q.dim())
+
+# Set up temperature field and initialise:
+T = Function(Q, name="Temperature")
+T_dev = Function(Q, name="Temperature_Deviation")
+X = SpatialCoordinate(mesh)
+r = sqrt(X[0]**2 + X[1]**2 + X[2]**2)
+theta = atan_2(X[1], X[0])  # Theta (longitude - different symbol to Zhong)
+phi = atan_2(sqrt(X[0]**2+X[1]**2), X[2])  # Phi (co-latitude - different symbol to Zhong)
+k = as_vector((X[0]/r, X[1]/r, X[2]/r))  # Radial unit vector (in direction opposite to gravity)
+
+conductive_term = rmin*(rmax - r) / (r*(rmax - rmin))
+# evaluate P_lm node-wise using scipy lpmv
+l, m, eps_c, eps_s = 3, 2, 0.01, 0.01
+Plm = Function(Q, name="P_lm")
+cos_phi = interpolate(cos(phi), Q)
+Plm.dat.data[:] = scipy.special.lpmv(m, l, cos_phi.dat.data_ro)
+Plm.assign(Plm*math.sqrt(((2*l+1)*math.factorial(l-m))/(2*math.pi*math.factorial(l+m))))
+if m == 0:
+    Plm.assign(Plm/math.sqrt(2))
+T.interpolate(conductive_term +
+              (eps_c*cos(m*theta) + eps_s*sin(m*theta)) * Plm * sin(pi*(r - rmin)/(rmax-rmin)))
+
+Ra = Constant(7e3)  # Rayleigh number
+approximation = BoussinesqApproximation(Ra)
+
+delta_t = Constant(1e-6)  # Initial time-step
+t_adapt = TimestepAdaptor(delta_t, V, maximum_timestep=0.1, increase_tolerance=1.5)
+
+# helper function to compute horizontal layer averages
+Tlayer = Function(Qlayer, name='LayerTemp')  # stores values of temp in one layer
+Tavg = Function(Q, name='LayerAveragedTemp')  # averaged temp function returned by function
+Rmin_area = assemble(Constant(1.0, domain=mesh2d)*dx)  # area of CMB
+
+
+def layer_average(T):
+    vnodes = nlayers*2 + 1  # n/o Q2 nodes in the vertical
+    hnodes = Qlayer.dim()  # n/o Q2 nodes in each horizontal layer
+    assert hnodes*vnodes == Q.dim()
+    for i in range(vnodes):
+        Tlayer.dat.data[:] = T.dat.data_ro[i::vnodes]
+        # NOTE: this integral is performed on mesh2d, which always has r=Rmin, but we normalize
+        Tavg.dat.data[i::vnodes] = assemble(Tlayer*dx) / Rmin_area
+    return Tavg
+
+
+# Define time stepping parameters:
+steady_state_tolerance = 1e-6
+max_timesteps = 20
+time = 0.0
+
+# Nullspaces and near-nullspaces:
+Z_nullspace = create_stokes_nullspace(Z, closed=True, rotational=True)
+Z_near_nullspace = create_stokes_nullspace(Z, closed=False, rotational=True, translations=[0, 1, 2])
+
+# Write output files in VTK format:
+u, p = z.split()  # Do this first to extract individual velocity and pressure fields.
+# Next rename for output:
+u.rename("Velocity")
+p.rename("Pressure")
+# Create output file and select output_frequency:
+output_file = File("output.pvd")
+dump_period = 1
+# Frequency of checkpoint files:
+checkpoint_period = dump_period * 4
+# Open file for logging diagnostic output:
+plog = ParameterLog('params.log', mesh)
+
+temp_bcs = {
+    bottom_id: {'T': 1.0},
+    top_id: {'T': 0.0},
+}
+stokes_bcs = {
+    bottom_id: {'un': 0},
+    top_id: {'un': 0},
+}
+
+energy_solver = EnergySolver(T, u, approximation, delta_t, ImplicitMidpoint, bcs=temp_bcs)
+Told = energy_solver.T_old
+Ttheta = 0.5*T + 0.5*Told
+Told.assign(T)
+stokes_solver = StokesSolver(z, Ttheta, approximation, delta_t, bcs=stokes_bcs,
+                             cartesian=False,
+                             nullspace=Z_nullspace, transpose_nullspace=Z_nullspace,
+                             near_nullspace=Z_near_nullspace)
+
+# Now perform the time loop:
+for timestep in range(0, max_timesteps):
+
+    # Write output:
+    if timestep % dump_period == 0:
+        # compute radial temperature
+        Tavg = layer_average(T)
+        # compute deviation from layer average
+        T_dev.assign(T-Tavg)
+        output_file.write(u, p, T, T_dev)
+
+    if timestep != 0:
+        dt = t_adapt.update_timestep(u)
+    else:
+        dt = float(delta_t)
+    time += dt
+
+    # Solve Stokes sytem:
+    stokes_solver.solve()
+
+    # Temperature system:
+    energy_solver.solve()
+
+    # Compute diagnostics:
+    u_rms = sqrt(assemble(dot(u, u) * dx)) * sqrt(1./domain_volume)
+    nusselt_number_top = (assemble(dot(grad(T), n) * ds_t) / assemble(Constant(1.0, domain=mesh)*ds_t)) * (rmax*(rmax-rmin)/rmin)
+    nusselt_number_base = (assemble(dot(grad(T), n) * ds_b) / assemble(Constant(1.0, domain=mesh)*ds_b)) * (rmin*(rmax-rmin)/rmax)
+    energy_conservation = abs(abs(nusselt_number_top) - abs(nusselt_number_base))
+    average_temperature = assemble(T * dx) / domain_volume
+
+    # Calculate L2-norm of change in temperature:
+    maxchange = sqrt(assemble((T - energy_solver.T_old)**2 * dx))
+
+    # Log diagnostics:
+    plog.log_str(f"{timestep} {time} {float(delta_t)} {maxchange} {u_rms} "
+                 f"{nusselt_number_base} {nusselt_number_top} "
+                 f"{energy_conservation} {average_temperature} ")
+
+    # Leave if steady-state has been achieved:
+    if maxchange < steady_state_tolerance:
+        log("Steady-state achieved -- exiting time-step loop")
+        break
+
+    # Checkpointing:
+    if timestep % checkpoint_period == 0:
+        # Checkpointing during simulation:
+        checkpoint_data = DumbCheckpoint(f"Temperature_State_{timestep}", mode=FILE_CREATE)
+        checkpoint_data.store(T, name="Temperature")
+        checkpoint_data.close()
+
+        checkpoint_data = DumbCheckpoint(f"Stokes_State_{timestep}", mode=FILE_CREATE)
+        checkpoint_data.store(z, name="Stokes")
+        checkpoint_data.close()
+
+plog.close()
+
+# Write final state:
+final_checkpoint_data = DumbCheckpoint("Final_Temperature_State", mode=FILE_CREATE)
+final_checkpoint_data.store(T, name="Temperature")
+final_checkpoint_data.close()
+
+final_checkpoint_data = DumbCheckpoint("Final_Stokes_State", mode=FILE_CREATE)
+final_checkpoint_data.store(z, name="Stokes")
+final_checkpoint_data.close()
