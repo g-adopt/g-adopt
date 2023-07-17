58a3d1211ff6ee969c5225f64426894ffc8b1c33
Rhodri Davies <rhodrid@Rhodris-Mac-Pro.local>
2023-07-17 19:49:18 +1000

    Remove Told and Ttheta from 3D cases
    Just parallel scaling left to do when that is eventually sorted
    through GAMG.

--- demos/3d_spherical/3d_spherical.py
+++ demos/3d_spherical/3d_spherical.py
@@ -109,10 +109,7 @@ stokes_bcs = {
 }
 
 energy_solver = EnergySolver(T, u, approximation, delta_t, ImplicitMidpoint, bcs=temp_bcs)
-Told = energy_solver.T_old
-Ttheta = 0.5*T + 0.5*Told
-Told.assign(T)
-stokes_solver = StokesSolver(z, Ttheta, approximation, bcs=stokes_bcs,
+stokes_solver = StokesSolver(z, T, approximation, bcs=stokes_bcs,
                              cartesian=False,
                              nullspace=Z_nullspace, transpose_nullspace=Z_nullspace,
                              near_nullspace=Z_near_nullspace)
