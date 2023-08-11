c00b568d31e89f46730acf2870985a2a89fc7af0
Angus Gibson <angus@agibson.me>
2023-08-11 10:19:55 +1000

    Slacken tolerance of 3d_cartesian
    Because this case is quite coarse in the first place, we're not quite
    hitting the strict default tolerance for equality.

--- demos/generate_expected.py
+++ demos/generate_expected.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if sys.argv[1:]:
         requested_cases = set(sys.argv[1:])
         cases = [
-            case for case in cases if
+            case for case in cases.keys() if
             case in requested_cases
         ]
 
