b0f97aa03c6989227d8ca6b044923f0e82fe0b8e
Angus Gibson <angus@agibson.me>
2023-08-19 22:21:05 +1000

    Add Helmholtz checkpointing case
    This tests whether we can resume ROL optimisation while retaining the
    same answers.

--- demos/adjoint/test_adjoint.py
+++ demos/adjoint/test_adjoint.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from pathlib import Path
 import taylor_test
 
@@ -9,3 +10,14 @@ def test_rectangular_taylor_test(case_name):
         minconv = float(f.read())
 
     assert minconv > 1.9
+
+
+def test_checkpointing():
+    base = Path(__file__).parent.resolve()
+
+    full_optimisation = np.loadtxt(base / "full_optimisation.dat")
+    restored_optimisation = np.loadtxt(base / "restored_optimisation.dat")
+
+    restored_steps = restored_optimisation.size
+
+    assert np.allclose(full_optimisation[-restored_steps:], restored_optimisation)
