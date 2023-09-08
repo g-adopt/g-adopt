84cf297cbd00e7b83912e8e975b294dbba8abe38
Rhodri Davies <Rhodri.Davies@anu.edu.au>
2023-09-08 14:00:08 +1000

    Lint.

--- gadopt/approximations.py
+++ gadopt/approximations.py
@@ -206,12 +206,9 @@ class AnelasticLiquidApproximation(TruncatedAnelasticLiquidApproximation):
     """
     Anelastic Liquid Approximation
 
-    Compressible approximation. Includes linear dependence of density on pressure (chi)"""    
+    Compressible approximation. Includes linear dependence of density on pressure (chi)"""
 
     def buoyancy(self, p, T):
         pressure_part = -self.Di * self.cp0 / self.cv0 / self.gamma0 * self.g * self.rho * self.chi * p
         temperature_part = super().buoyancy(p, T)
         return pressure_part + temperature_part
-    
-    
-    
