4d49c18f2b54ae5430147fedb5bb7e70b9bdcaf4
w3168 <wis15@ic.ac.uk>
2023-09-20 18:13:27 +1000

    Rewrite Donea Fig2.7 example with a function for error testing
    errors compared with Donea and Huerta 2003 equation 2.23 for
    the steady advection diffusion solution with constant source term,
    constant velocity and homogenous dirichlet bcs on both sides
    
    running with a grid peclet number of 50 and initial mesh resolution of 5 cells gives these results
    SU_upwind =  [0.16548618, 0.11845977, 0.08582658, 0.06341912, 0.0456253,  0.02835158, 0.01551193, 0.00813498]  # i.e set Beta(Pe) = 1.
    SU = [0.16343463, 0.11554859, 0.0816918,  0.05753857, 0.03732994, 0.0180603, 0.00614359, 0.00170367]
    noSU = [0.57030194, 1.08824244, 0.22152179, 0.09494632, 0.04086379, 0.01557628,  0.00484643, 0.00130424]
    
    running with a grid peclet number of 0.25 and initial mesh resolution of 5 cells gives these results
    SU_fromPe0pt25 = [0.01091291, 0.00276109, 0.00069343, 0.00017465]
    noSU_fromPe0pt25 = [0.00863846, 0.00217787, 0.00054636, 0.00013744]
    SU_upwind_fromPe0pt25 = [0.04179666, 0.02128963, 0.01080482, 0.00545314] # i.e. set Beta(Pe) =1
    
    at higher grid peclet numbers (greater than about 10) both upwinding
    and su are pretty much the same (because the Be(Pe) ->1 = upwinding)
    and have lower errors than galerkin with no su (due to oscillations).
    
    The convergence for SU and upwinding seems to be about 0.5 at this stage.
    I couldnt find a theoretical reason why but it kind of makes sense
    that you are not in the asymptotic limit because both methods add extra diffusion
    based on the grid cell size, so as you refine the mesh you are effectively
    changing the problem you are solving, compared with the analytical solution
    that has a fixed specified diffusivity.
    
    As the grid peclet number goes below 1 the convergence of SU and
    galerkin both approaches 2 which makes sense for a P1 discretisation.
    The convergence of the upwinding method approaches 1 which also makes
    sense because you are no longer using a central difference type approximation.

--- demos/scalar_advection_diffusion/scalar_advection_diffusion_DH27.py
+++ demos/scalar_advection_diffusion/scalar_advection_diffusion_DH27.py
@@ -1,64 +1,110 @@
-# Demo for scalar advection-diffusion based on Figure 2.7 in
-# Chapter 2 Steady transport problems from Finite element Methods
-# for Flow problems - Donea and Huerta, 2003
 
 from gadopt import *
 from gadopt.scalar_equation import ScalarAdvectionDiffusionEquation
 from gadopt.time_stepper import DIRK33
 
-mesh = UnitSquareMesh(10, 10, quadrilateral=True)
+def model(n, do_write=False):
+    """ Demo for scalar advection-diffusion based on Figure 2.7 in
+    Chapter 2 Steady transport problems from Finite element Methods
+    for Flow problems - Donea and Huerta, 2003
 
-# We set up a function space of discontinous bilinear elements for :math:`q`, and
-# a vector-valued continuous function space for our velocity field. ::
+    Args: 
+        n: number of grid cells in x and y direction
+        do_write: whether to output the scalar/velocity field
+    """
+    mesh = UnitSquareMesh(n, n, quadrilateral=True)
 
-V = FunctionSpace(mesh, "CG", 1)
-W = VectorFunctionSpace(mesh, "CG", 1)
+    # We set up a function space of discontinous bilinear elements for :math:`q`, and
+    # a vector-valued continuous function space for our velocity field. ::
 
-# We set up the initial velocity field using a simple analytic expression. ::
+    V = FunctionSpace(mesh, "CG", 1)
+    V2 = FunctionSpace(mesh, "CG", 2)
+    W = VectorFunctionSpace(mesh, "CG", 1)
 
-x = SpatialCoordinate(mesh)
-velocity = as_vector((1, 0))
-u = Function(W).interpolate(velocity)
-File('u.pvd').write(u)
+    # We set up the initial velocity field using a simple analytic expression. ::
 
-# the diffusivity
-Pe = 5
-h = 0.1
-kappa = Constant(1*h/(2*Pe))
+    x = SpatialCoordinate(mesh)
+    a = Constant(1)
+    velocity = as_vector((a, 0))
+    u = Function(W).interpolate(velocity)
+    if do_write:
+        File('u.pvd').write(u)
 
-
-# the tracer function and its initial condition
-q_init = Constant(0.0)
-q = Function(V).interpolate(q_init)
-
-# We declare the output filename, and write out the initial condition. ::
-
-outfile = File("advdif_DH2.7_CG1_Pe"+str(Pe)+"_SU.pvd")
-outfile.write(q)
-
-# time period and time step
-T = 10.
-dt = 0.01
-
-eq = ScalarAdvectionDiffusionEquation(V, V, su_advection=True)
-fields = {'velocity': u, 'diffusivity': kappa, 'source': 1.0}
-# weakly applied dirichlet bcs on top and bottom
-q_left = 0.0
-q_right = 0.0
-strong_bcs = DirichletBC(V, 0, [1, 2])
-bcs = {1: {'q': q_left}, 2: {'q': q_right}}
-timestepper = DIRK33(eq, q, fields, dt, strong_bcs=strong_bcs)
+    # the diffusivity
+    Pe = 0.25  # peclet number for coarsest grid
+    h = 1/5 
+    kappa = Constant(1*h/(2*Pe))
 
 
-t = 0.0
-step = 0
-while t < T - 0.5*dt:
-    # the solution reaches a steady state and finishes the solve when a  max no. of iterations is reached
-    timestepper.advance(t)
+    # the tracer function and its initial condition
+    q_init = Constant(0.0)
+    q = Function(V).interpolate(q_init)
 
-    step += 1
-    t += dt
+    # We declare the output filename, and write out the initial condition. ::
 
-    if step % 1 == 0:
-        outfile.write(q)
+    outfile = File("advdif_DH2.7_CG1_Pe"+str(Pe)+"_SU.pvd")
+    outfile.write(q)
+
+    # time period and time step
+    T = 10.
+    dt = 0.01
+
+    eq = ScalarAdvectionDiffusionEquation(V, V, su_advection=True)
+    fields = {'velocity': u, 'diffusivity': kappa, 'source': 1.0}
+    # weakly applied dirichlet bcs on top and bottom
+    q_left = 0.0
+    q_right = 0.0
+    strong_bcs = DirichletBC(V, 0, [1, 2])
+    bcs = {1: {'q': q_left}, 2: {'q': q_right}}
+    timestepper = DIRK33(eq, q, fields, dt, strong_bcs=strong_bcs)
+    
+    steady_state_tolerance = 1e-7
+    t = 0.0
+    step = 0
+
+    while t < T - 0.5*dt:
+        # the solution reaches a steady state and finishes the solve when a  max no. of iterations is reached
+        timestepper.advance(t)
+        
+        # Calculate L2-norm of change in temperature:
+        maxchange = sqrt(assemble((q - timestepper.solution_old)**2 * dx))
+        print("maxchange", maxchange)
+        step += 1
+        t += dt
         print("t=", t)
+
+        if do_write:
+            outfile.write(q)
+
+        if maxchange < steady_state_tolerance:
+            print("Steady-state acheieved -- exiting time-step loop")
+            break
+
+
+    # analytical solution from equation 2.23 in Chapter 2 Steady transport problems 
+    # from Finite element Methods for Flow problems - Donea and Huerta, 2003
+    # N.b they have the scalar called 'u' whereas we have 'q'
+        gamma = Constant(a/kappa)
+        q_anal = Function(V2)
+        q_anal.interpolate((1/a) * (x[0] - (1 - exp(gamma*x[0]))/(1-exp(gamma))))  
+        
+        L2anal_q = norm(q)
+        L2error_q = errornorm(q_anal, q, norm_type='L2')
+        
+        print(norm(q_anal))
+
+        print("L2_anal_q", L2anal_q)
+        print("L2_error_q", L2error_q)
+    
+    outfile.write(q)
+    ana_outfile = File("advdif_DH2.7_CG1_Pe"+str(Pe)+"_SU_ana.pvd")
+    ana_outfile.write(q_anal)
+
+    return L2error_q
+import numpy as np
+nxs = [5*(2**i) for i in range(4)] #, 0.00625, 0.003125]#, 0.125, 0.0625, 0.03125]
+errors = np.array([model(nx) for nx in nxs]) 
+conv = np.log(errors[:-1]/errors[1:])/np.log(2)
+
+print('time surface displacement errors: ', errors[:])
+print('time surface displacement conv: ', conv[:])
