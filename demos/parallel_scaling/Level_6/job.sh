c65cb3cc877d62c0b526bf14bdf30c2a4ff7e6e2
Rhodri Davies <Rhodri.Davies@anu.edu.au>
2023-10-13 10:21:15 +1100

    Add correct job scripts.

--- demos/parallel_scaling/Level_6/job.sh
+++ demos/parallel_scaling/Level_6/job.sh
@@ -8,15 +8,25 @@
 #PBS -l jobfs=800GB
 #PBS -l storage=scratch/xd2+gdata/xd2
 #PBS -l wd
+#PBS -W umask=0022
+openmpi_version=4.0.7
+petsc_arch=opt
 
-module use /g/data/xd2/modulefiles
-module load firedrake-singularity/20230307
-export PYTHONPATH=$PYTHONPATH:/g/data/xd2/rad552/FIREDRAKE_GIT/gadopt-restructure
+module load python3/3.10.4 # intel-compiler/2021.10.0
+module load openmpi/${openmpi_version}
+
+export PETSC_DIR=/tmp/firedrake-prefix
 export OMP_NUM_THREADS=1
+
+mpiexec --map-by ppr:1:node -np $PBS_NNODES tar -C $PBS_JOBFS -xf /g/data/xd2/ahg157/firedrake-prefixes/firedrake-prefix-20231012-gamg-openmpi407-opt.tar.gz
+mpiexec --map-by ppr:1:node -np $PBS_NNODES ln -s $PBS_JOBFS/firedrake-prefix /tmp/firedrake-prefix
+
+export PYTHONUSERBASE=/tmp/firedrake-prefix
+export XDG_CACHE_HOME=$PBS_JOBFS/xdg
+
+export PYTHONPATH="/scratch/xd2/rad552/FIREDRAKE_GIT/g-adopt"
 export PETSC_OPTIONS="-log_view"
 
-mpiexec --map-by ppr:1:node -np $PBS_NNODES cp /g/data/xd2/modules/firedrake-singularity/20230307/firedrake $PBS_JOBFS
-
 #### Now run
-mpiexec -np $PBS_NCPUS $PBS_JOBFS/firedrake stokes_cubed_sphere_7e3_A3_TS1.py &> output_1.dat &&
-mpiexec -np $PBS_NCPUS $PBS_JOBFS/firedrake stokes_cubed_sphere_7e3_A3.py &> output_2.dat
+mpiexec python3 stokes_cubed_sphere_7e3_A3_TS1.py &> output_1.dat &&
+mpiexec python3 stokes_cubed_sphere_7e3_A3.py &> output_2.dat
