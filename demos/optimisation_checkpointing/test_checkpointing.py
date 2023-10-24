f26623d503e1e801cb0e768439761463b25110db
rubyturner <ruby.turner@anu.edu.au>
2023-10-24 12:22:16 +1100

    Change default restored checkpoint in optimisation to the last saved (#39)
    * Change default restored checkpoint in optimisation to the last saved
    
    * Update checkpoint tests

--- demos/optimisation_checkpointing/test_checkpointing.py
+++ demos/optimisation_checkpointing/test_checkpointing.py
@@ -6,8 +6,12 @@ def test_checkpointing():
     base = Path(__file__).parent.resolve()
 
     full_optimisation = np.loadtxt(base / "full_optimisation.dat")
-    restored_optimisation = np.loadtxt(base / "restored_optimisation.dat")
+    restored_optimisation = np.loadtxt(base / "restored_optimisation_from_it_5.dat")
 
     restored_steps = restored_optimisation.size
 
     assert np.allclose(full_optimisation[-restored_steps:], restored_optimisation)
+
+    restored_optimisation_last_it = np.loadtxt(base / "restored_optimisation_from_last_it.dat")
+
+    assert full_optimisation[-1] > restored_optimisation_last_it[0]
