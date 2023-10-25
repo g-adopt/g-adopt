70ae94c6023ac9165d7fcaf99aa04239d11a10d9
Thomas Duvernay <td75013@hotmail.fr>
2023-10-25 16:17:07 +1100

    ENH: Various syntax and comment improvements

--- demos/multi_material/van_keken/level_set.py
+++ demos/multi_material/van_keken/level_set.py
@@ -17,18 +17,25 @@ from gadopt.time_stepper import SSPRK33
 from gadopt.utility import TimestepAdaptor
 
 
-def initial_signed_distance(lx, node_coords_x, node_coords_y):
-    interface_x = np.linspace(0, lx, 1000)
-    interface_y = 0.2 + 0.02 * np.cos(np.pi * interface_x / 0.9142)
+def initial_signed_distance():
+    """Determine the signed distance to the material interface for each mesh node"""
+
+    def get_interface_y(interface_x):
+        return layer_interface_y + interface_deflection * np.cos(
+            np.pi * interface_x / domain_length_x
+        )
+
+    interface_x = np.linspace(0, domain_length_x, 1000)
+    interface_y = get_interface_y(interface_x)
     curve = sl.LineString([*np.column_stack((interface_x, interface_y))])
     sl.prepare(curve)
 
     node_relation_to_curve = [
         (
-            y > 0.2 + 0.02 * np.cos(np.pi * x / 0.9142),
-            curve.distance(sl.Point(x, y)),
+            node_coord_y > get_interface_y(node_coord_x),
+            curve.distance(sl.Point(node_coord_x, node_coord_y)),
         )
-        for x, y in zip(node_coords_x, node_coords_y)
+        for node_coord_x, node_coord_y in zip(node_coords_x, node_coords_y)
     ]
     node_sign_dist_to_curve = [
         dist if is_above else -dist for is_above, dist in node_relation_to_curve
@@ -37,15 +44,18 @@ def initial_signed_distance(lx, node_coords_x, node_coords_y):
 
 
 # Set up geometry
-nx, ny = 64, 64
-lx, ly = 0.9142, 1
-mesh = fd.RectangleMesh(nx, ny, lx, ly, quadrilateral=True)
-left_id, right_id, bottom_id, top_id = 1, 2, 3, 4  # Mesh boundary IDs
-
+mesh_elements_x, mesh_elements_y = 64, 64
+domain_length_x, domain_length_y = 0.9142, 1
+mesh = fd.RectangleMesh(
+    mesh_elements_x,
+    mesh_elements_y,
+    domain_length_x,
+    domain_length_y,
+    quadrilateral=True,
+)
 mesh_coords = fd.SpatialCoordinate(mesh)
 
-conservative_level_set = True
-
+# Set up function spaces and functions used as part of the level-set approach
 level_set_func_space_deg = 2
 func_space_dg = fd.FunctionSpace(mesh, "DQ", level_set_func_space_deg)
 vec_func_space_cg = fd.VectorFunctionSpace(mesh, "CG", level_set_func_space_deg)
@@ -54,69 +64,85 @@ level_set_grad_proj = fd.Function(vec_func_space_cg, name="level_set_grad_proj")
 node_coords_x = fd.Function(func_space_dg).interpolate(mesh_coords[0]).dat.data
 node_coords_y = fd.Function(func_space_dg).interpolate(mesh_coords[1]).dat.data
 
-node_sign_dist_to_interface = initial_signed_distance(lx, node_coords_x, node_coords_y)
+# Initial interface layout
+layer_interface_y = domain_length_y / 5
+interface_deflection = domain_length_y / 50
+signed_dist_to_interface = initial_signed_distance()
 
+# Initialise level set
+conservative_level_set = True
 if conservative_level_set:
-    epsilon = fd.Constant(min(lx / nx, ly / ny) / 4)  # Loose guess
+    epsilon = fd.Constant(
+        min(domain_length_x / mesh_elements_x, domain_length_y / mesh_elements_y) / 4
+    )  # Loose guess
     level_set.dat.data[:] = (
-        np.tanh(np.asarray(node_sign_dist_to_interface) / 2 / epsilon.values().item())
-        + 1
+        np.tanh(np.asarray(signed_dist_to_interface) / 2 / epsilon.values().item()) + 1
     ) / 2
     level_set_contour = 0.5
 else:
-    level_set.dat.data[:] = node_sign_dist_to_interface
+    level_set.dat.data[:] = signed_dist_to_interface
     level_set_contour = 0
 
-# Set up Stokes function spaces - currently using the bilinear Q2Q1 element pair:
-V = fd.VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
-W = fd.FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
-Z = fd.MixedFunctionSpace([V, W])  # Mixed function space
+# Set up Stokes function spaces corresponding to the bilinear Q2Q1 element pair
+vel_func_space = fd.VectorFunctionSpace(mesh, "CG", 2)
+pres_func_space = fd.FunctionSpace(mesh, "CG", 1)
+stokes_func_space = fd.MixedFunctionSpace([vel_func_space, pres_func_space])
 
-z = fd.Function(Z)  # Field over the mixed function space Z
-u, p = fd.split(z)  # Symbolic UFL expression for u and p
+stokes_function = fd.Function(stokes_func_space)
+u, p = fd.split(stokes_function)  # Symbolic UFL expression for velocity and pressure
 
-# Parameters; since they are included in UFL, they are wrapped inside Constant
+# Physical parameters; since they are included in UFL, they are wrapped inside Constant
 g = fd.Constant(10)
 Ra = fd.Constant(-1)
 T = fd.Constant(1)
 
+# Physical fields depending on the level set
 rho = fd.conditional(level_set > level_set_contour, 1 / g, 0 / g)
 mu = fd.conditional(level_set > level_set_contour, 1, 1)
 
+# Set up Stokes solver
 approximation = BoussinesqApproximation(Ra, g=g, rho=rho)
-Z_nullspace = create_stokes_nullspace(Z)
+stokes_nullspace = create_stokes_nullspace(stokes_func_space)
 stokes_bcs = {
-    bottom_id: {"ux": 0, "uy": 0},
-    top_id: {"ux": 0, "uy": 0},
-    left_id: {"ux": 0},
-    right_id: {"ux": 0},
-}
+    1: {"ux": 0},
+    2: {"ux": 0},
+    3: {"ux": 0, "uy": 0},
+    4: {"ux": 0, "uy": 0},
+}  # RectangleMesh boundary mapping: {1: left, 2: right, 3: bottom, 4: top}
 stokes_solver = StokesSolver(
-    z,
+    stokes_function,
     T,
     approximation,
     bcs=stokes_bcs,
     mu=mu,
-    nullspace=Z_nullspace,
-    transpose_nullspace=Z_nullspace,
+    nullspace=stokes_nullspace,
+    transpose_nullspace=stokes_nullspace,
 )
 
+# Set up timestepping objects
 dt = fd.Constant(1)  # Initial time-step
-# Empirical calibration
-target_cfl = 2 * (ly / ny) ** (1 / 10) / (level_set_func_space_deg + 1) ** (5 / 3)
-t_adapt = TimestepAdaptor(dt, u, V, target_cfl=target_cfl, maximum_timestep=5)
+target_cfl = (
+    2
+    * (domain_length_y / mesh_elements_y) ** (1 / 10)
+    / (level_set_func_space_deg + 1) ** (5 / 3)
+)  # Empirical calibration
+t_adapt = TimestepAdaptor(
+    dt, u, vel_func_space, target_cfl=target_cfl, maximum_timestep=5
+)
 
+# Additional fields required for each level-set solver
 level_set_fields = {"velocity": u}
 projection_fields = {"target": level_set}
 reinitialisation_fields = {"level_set_grad": level_set_grad_proj, "epsilon": epsilon}
 
-# Forces gradient norm to satisfy reinitialisation equation
+# Force projected gradient norm to satisfy the reinitialisation equation
 projection_boundary_conditions = fd.DirichletBC(
     vec_func_space_cg,
     level_set * (1 - level_set) / epsilon * fd.Constant((1, 0)),
     "on_boundary",
 )
 
+# Set up level-set solvers
 level_set_solver = TimeStepperSolver(
     level_set, level_set_fields, dt, SSPRK33, LevelSetEquation
 )
@@ -132,15 +158,17 @@ reinitialisation_solver = TimeStepperSolver(
     coupled_solver=projection_solver,
 )
 
+# Time-loop objects
 time_now, time_end = 0, 2000
 dump_counter, dump_period = 0, 10
 output_file = fd.File("level_set/output.pvd", target_degree=level_set_func_space_deg)
 
 # Extract individual velocity and pressure fields and rename them for output
-u_, p_ = z.subfunctions
+u_, p_ = stokes_function.subfunctions
 u_.rename("Velocity")
 p_.rename("Pressure")
 
+# Relevant post-processing fields
 output_time = []
 rms_velocity = []
 entrainment = []
@@ -150,39 +178,48 @@ step = 0
 reini_frequency = 1
 reini_iterations = 5
 while time_now < time_end:
-    if time_now >= dump_counter * dump_period:
+    if time_now >= dump_counter * dump_period:  # Write to output file
         dump_counter += 1
         output_file.write(level_set, u_, p_, level_set_grad_proj)
 
+    # Update time and timestep
     dt = t_adapt.update_timestep()
     time_now += dt
 
+    # Solve Stokes system
     stokes_solver.solve()
 
+    # Solve level-set advection
     level_set_solver.solve()
 
-    if step > reini_frequency:
+    if step > reini_frequency:  # Update stored function given previous solve
         reinitialisation_solver.ts.solution_old.assign(level_set)
 
-    if step > 0 and step % reini_frequency == 0:
+    if step > 0 and step % reini_frequency == 0:  # Solve level-set reinitialisation
         if conservative_level_set:
             for reini_step in range(reini_iterations):
                 reinitialisation_solver.solve()
 
+        # Update stored function given previous solve
         level_set_solver.ts.solution_old.assign(level_set)
 
+    # Calculate and store post-processing fields
     output_time.append(time_now)
     rms_velocity.append(fd.norm(u) / fd.sqrt(domain_volume(mesh)))
     entrainment.append(
-        fd.assemble(fd.conditional(mesh_coords[1] >= 0.2, 1 - rho * g, 0) * fd.dx)
-        / lx
-        / 0.2
+        fd.assemble(
+            fd.conditional(mesh_coords[1] >= layer_interface_y, 1 - rho * g, 0) * fd.dx
+        )
+        / domain_length_x
+        / layer_interface_y
     )
 
+    # Increment time-loop step counter
     step += 1
+# Write final simulation state to output file
 output_file.write(level_set, u_, p_, level_set_grad_proj)
 
-if MPI.COMM_WORLD.rank == 0:
+if MPI.COMM_WORLD.rank == 0:  # Save post-processing fields and produce graphs
     np.savez(
         "output", time=output_time, rms_velocity=rms_velocity, entrainment=entrainment
     )
