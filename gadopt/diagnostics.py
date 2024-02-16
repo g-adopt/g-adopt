7abacbcfcb8748c444844e37514991ba002ad1f0
Angus Gibson <angus@agibson.me>
2024-02-16 12:12:06 +1100

    Reformat diagnostics.py documentation

--- gadopt/diagnostics.py
+++ gadopt/diagnostics.py
@@ -1,14 +1,32 @@
 import firedrake
-from firedrake import Constant, FacetNormal, Function, assemble, dot, grad, sqrt, ufl
+from firedrake import assemble, Constant, Function, sqrt, dot, grad, FacetNormal
 from firedrake.ufl_expr import extract_unique_domain
-
 from .utility import CombinedSurfaceMeasure
-
-__all__ = ["GeodynamicalDiagnostics"]
 
 
 class GeodynamicalDiagnostics:
-    """Typical simulation diagnostics used in geodynamical simulations."""
+    """Typical simulation diagnostics used in geodynamical simulations.
+
+    Arguments:
+      u:         Firedrake function for the velocity
+      p:         Firedrake function for the pressure
+      T:         Firedrake function for the temperature
+      bottom_id: bottom boundary identifier.
+      top_id:    top boundary identifier.
+      degree:    degree of the polynomial approximation.
+
+    Note:
+      All the diagnostics are returned as a float value.
+
+    Functions:
+      domain_volume: The numerical domain's volume
+      u_rms: Root-mean squared velocity
+      u_rms_top: Root-mean squared velocity along the top boundary
+      Nu_top: Nusselt number at the top boundary
+      Nu_bottom: Nusselt number at the bottom boundary
+      T_avg: Average temperature in the domain
+
+    """
 
     def __init__(
         self,
@@ -19,22 +37,6 @@ class GeodynamicalDiagnostics:
         top_id: int,
         degree: int = 4
     ):
-        """Initialises the diagnostics instance from the simulation's state.
-
-        Args:
-          u:
-            Firedrake function for the velocity.
-          p:
-            Firedrake function for the pressure.
-          T:
-            Firedrake function for the temperature.
-          bottom_id:
-            Integer for the bottom boundary identifier.
-          top_id:
-            Integer for the top boundary identifier.
-          degree:
-            Integer specifying the degree of the polynomial approximation.
-        """
         self.mesh = extract_unique_domain(u)
         self.u = u
         self.p = p
@@ -50,52 +52,20 @@ class GeodynamicalDiagnostics:
 
         self.n = FacetNormal(self.mesh)
 
-    def domain_volume(self) -> ufl.core.expr.Expr:
-        """Numerical domain area or volume.
-
-        Returns:
-          A UFL expression to calculate the numerical domain's volume.
-        """
+    def domain_volume(self) -> float:
         return assemble(Constant(1)*firedrake.dx(domain=self.mesh))
 
-    def u_rms(self) -> ufl.core.expr.Expr:
-        """Root-mean squared velocity.
-
-        Returns:
-          A UFL expression to calculate the root-mean squared velocity.
-        """
+    def u_rms(self) -> float:
         return sqrt(assemble(dot(self.u, self.u) * self.dx)) * sqrt(1./self.domain_volume())
 
-    def u_rms_top(self) -> ufl.core.expr.Expr:
-        """Root-mean squared velocity along the top boundary.
-
-        Returns:
-          A UFL expression to calculate the root-mean squared velocity along the
-          domain's top boundary.
-        """
+    def u_rms_top(self) -> float:
         return sqrt(assemble(dot(self.u, self.u) * self.ds_t))
 
-    def Nu_top(self) -> ufl.core.expr.Expr:
-        """Nusselt number at the top boundary.
-
-        Returns:
-          A UFL expression to calculate the Nusselt number at the domain's top boundary.
-        """
+    def Nu_top(self) -> float:
         return -1 * assemble(dot(grad(self.T), self.n) * self.ds_t) * (1./assemble(Constant(1) * self.ds_t))
 
-    def Nu_bottom(self) -> ufl.core.expr.Expr:
-        """Nusselt number at the bottom boundary.
-
-        Returns:
-          A UFL expression to calculate the Nusselt number at the domain's bottom
-          boundary.
-        """
+    def Nu_bottom(self) -> float:
         return assemble(dot(grad(self.T), self.n) * self.ds_b) * (1./assemble(Constant(1) * self.ds_b))
 
-    def T_avg(self) -> ufl.core.expr.Expr:
-        """Average temperature.
-
-        Returns:
-          A UFL expression to calculate the average temperature throughout the domain.
-        """
+    def T_avg(self) -> float:
         return assemble(self.T * self.dx) / self.domain_volume()
