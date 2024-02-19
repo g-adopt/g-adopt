1c5116a40e4c93338dbd2ef20b05c3d2194ddaea
w3168 <william.scott1@anu.edu.au>
2024-02-19 16:33:36 +1100

    Forgot to add viscoelastic_equation module

---
+++ gadopt/viscoelastic_equation.py
@@ -0,0 +1,48 @@
+from .equations import BaseTerm
+from .momentum_equation import MomentumEquation
+from firedrake import dot, FacetNormal
+r"""
+This module contains the additional terms and equations necessary for viscoelasticity
+
+NOTE: for all terms, the residual() method returns the residual as it would be on the RHS of the equation, i.e.:
+
+  dq/dt = \sum term.residual()
+
+This sign-convention is for compatibility with Thetis' timeintegrators. In general, however we like to think about
+the terms as they are on the LHS. Therefore in the residual methods below we assemble in F as it would be on the LHS:
+
+  dq/dt + F(q) = 0
+
+and at the very end "return -F".
+"""
+
+
+class PreviousStressTerm(BaseTerm):
+
+    r"""
+    Previous stress term :math:`-\nabla \cdot (\mu \nabla u)`
+
+    """
+    def residual(self, test, trial, trial_lagged, fields, bcs):
+
+        previous_stress = fields['previous_stress']
+        phi = test
+        grad_test = nabla_grad(phi)
+
+        F = inner(grad_test, previous_stress)*self.dx
+
+        return -F  
+
+
+class ViscoelasticEquation(MomentumEquation):
+    """
+    Viscoelastic Equation.
+    """
+
+    terms.append(PreviousStressTerm)
+
+
+def ViscoElasticEquations(test_space, trial_space, quad_degree=None, **kwargs):
+    mom_eq = ViscoElasticEquation(test_space.sub(0), trial_space.sub(0), quad_degree=quad_degree, **kwargs)
+    cty_eq = ContinuityEquation(test_space.sub(1), trial_space.sub(1), quad_degree=quad_degree, **kwargs)
+    return [mom_eq, cty_eq]
