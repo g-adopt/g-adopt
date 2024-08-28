fec7239dda9beea5f95d99e43edd55923f194423
Angus Gibson <angus@agibson.me>
2024-08-28 18:43:30 +1000

    Address warnings in tutorials (#104)
    There are a few irrelevant warnings in our tutorials (an import warning
    from Firedrake, interpolation, plotting, etc.) that we can improve upon.
    There is one outstanding warning from the gplates demo:
    ```
    /home/firedrake/firedrake/lib/python3.12/site-packages/gadopt/gplates/gplates.py:407: RuntimeWarning: Issues finding plate ids for some of the seeds.
      warnings.warn("Issues finding plate ids for some of the seeds.", category=RuntimeWarning)
    ```

--- demos/gplates_global/gplates_global.py
+++ demos/gplates_global/gplates_global.py
@@ -28,6 +28,10 @@
 # Let's begin with the usual import of G-ADOPT, set up of the mesh,
 # function spaces, functions to hold our solutions, material
 # properties, approximations and initial conditions:
+#
+# Note that sometimes we get a confusing `SyntaxWarning` during the
+# set up. This is due to the pyGPlates module itself, but doesn't
+# affect any functionality and is safe to ignore.
 
 # +
 from gadopt import *
@@ -95,6 +99,11 @@ averager.extrapolate_layer_average(T_avg, averager.get_layer_average(T))
 # space. The 1-D profile data is located in the file
 # `./mu2_radial.rad`, and we will use interpolation functionalities
 # provided by G-ADOPT to populate the viscosity field.
+#
+# This expression generates a warning from TSFC, the Two-Stage Form
+# Compiler. The underlying interpolation routine on a non-Cartesian
+# geometry hits an edge case in the form optimisation, but this can
+# safely be ignored.
 
 mu = Function(Q, name="Viscosity")
 interpolate_1d_profile(function=mu, one_d_filename="mu2_radial.rad")
@@ -215,10 +224,15 @@ gplates_velocities = GplatesVelocityFunction(
 #     vtk_file.write(gplates_velocities)
 #
 # import pyvista as pv
+# import os
 # dataset = pv.read("gplates_velocity/gplates_velocity_0.vtu")
 #
 # # Create a plotter object
 # plotter = pv.Plotter()
+# # Whether our plot should be interactive or not
+# backend = None
+# if os.environ.get("GADOPT_RENDER", "false").lower() == "true":
+#     backend = "static"
 # # Add the dataset to the plotter
 # plotter.add_mesh(dataset, scalars="GPlates_Velocity", cmap="coolwarm")
 # glyphs = dataset.glyph(orient="GPlates_Velocity", scale=1, factor=1e-4)
@@ -227,7 +241,7 @@ gplates_velocities = GplatesVelocityFunction(
 # # Adjust the camera position
 # plotter.camera_position = [(10.0, 10.0, 10.0), (0.0, 0.0, 0), (0, 1, 0)]
 # # Show the plot
-# plotter.show()
+# plotter.show(jupyter_backend=backend)
 # -
 
 # And last but not least, we need to inform our solver of our choice
