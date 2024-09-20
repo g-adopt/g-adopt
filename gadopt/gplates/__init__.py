c0d298688b0c1fbf5f4ad92c550dc90d3fa93018
Sia Ghelichkhan <siavash.ghelichkhan@anu.edu.au>
2024-09-20 14:10:26 +1000

    Allow for more generic configuration of plate reconstruction datasets (#109)
    This will provide access to multiple plate reconstruction models .
    
    ---------
    
    Co-authored-by: Angus Gibson <angus@agibson.me>

--- gadopt/gplates/__init__.py
+++ gadopt/gplates/__init__.py
@@ -1,5 +1,5 @@
 from .gplates import GplatesVelocityFunction, pyGplatesConnector
-from .gplatesfiles import obtain_Muller_2022_SE
+from .gplatesfiles import ensure_reconstruction
 
 
-__all__ = ['GplatesVelocityFunction', 'pyGplatesConnector', 'obtain_Muller_2022_SE']
+__all__ = ["GplatesVelocityFunction", "pyGplatesConnector", "ensure_reconstruction"]
