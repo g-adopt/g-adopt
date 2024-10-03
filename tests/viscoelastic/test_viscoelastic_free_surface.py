3f9e11b60645f411669d9280525eee21bff0f1c7
w3168 <wis15@ic.ac.uk>
2024-10-03 18:57:21 +1000

    Add analytical viscoelastic loading test
    Based on Cathles et al 2024 Equation 2. The test here is loading
    instead of unloading so we don't have to run to isostatic equilibrium
    to begin with.
    
    Although zhong's method claims to be second order we only see first order
    convergence. I believe this is due to errors in the free surface dominating
    not errors in timestepping the constitutive equation, based on
    experiments with velocity formulation (moresi).

---
+++ tests/viscoelastic/test_viscoelastic_free_surface.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+base = Path(__file__).parent.resolve()
+
+
+@pytest.fixture
+def expected_errors():
+    return np.load(base / "expected_errors.npz")
+
+
+cases = [
+    ("viscoelastic-zhong", [1.0, 1.0, 1.0]),
+]
+
+
+@pytest.mark.parametrize("case_name,expected_convergence", cases)
+def test_viscoelastic_free_surface(case_name, expected_convergence, expected_errors):
+    expected_errors = expected_errors[case_name]
+    errors = np.loadtxt(base / f"errors-{case_name}-free-surface.dat")
+
+    # check that norm(q) is the same as previously run
+    assert np.allclose(errors, expected_errors, rtol=1e-6, atol=1e-16)
+
+    # use the highest resolution analytical solutions as the reference in scaling
+    ref = errors[-1]
+    relative_errors = errors / ref
+    convergence = np.log2(relative_errors[:-1] / relative_errors[1:])
+    assert np.allclose(convergence, expected_convergence, rtol=1e-1)
---
+++ tests/viscoelastic/zhong_viscoelastic_free_surface.py
@@ -0,0 +1,141 @@
+from gadopt import *
+import numpy as np
+OUTPUT = False
+output_directory = "./2d_analytic_zhong_viscoelastic_freesurface/"
+
+
+def viscoelastic_model(nx, dt_factor):
+    # Set up geometry:
+    nz = nx  # Number of vertical cells
+    D = 3e6  # length of domain in m
+    L = D/2  # Depth of the domain in m
+    mesh = RectangleMesh(nx, nz, L, D)  # Rectangle mesh generated via firedrake
+    mesh.cartesian = True
+
+    # Squash mesh to refine near top boundary modified from the ocean model
+    # Roms e.g. https://www.myroms.org/wiki/Vertical_S-coordinate
+    mesh.coordinates.dat.data[:, 1] -= D
+    x, z = SpatialCoordinate(mesh)
+    a = Constant(4)
+    b = Constant(0)
+    depth_c = 500.0
+    z_scaled = z / D
+    Cs = (1.-b) * sinh(a*z_scaled) / sinh(a) + b*(tanh(a*(z_scaled + 0.5))/(2*tanh(0.5*a)) - 0.5)
+    Vc = mesh.coordinates.function_space()
+    f = Function(Vc).interpolate(as_vector([x, depth_c*z_scaled + (D - depth_c)*Cs]))
+    mesh.coordinates.assign(f)
+    left_id, right_id, bottom_id, top_id = 1, 2, 3, 4  # Boundary IDs
+
+    # Set up function spaces - currently using P2P1 element pair:
+    V = VectorFunctionSpace(mesh, "CG", 2)  # Displacement function space (vector)
+    W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+    Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+    Z = MixedFunctionSpace([V, W])  # Mixed function space.
+    TP1 = TensorFunctionSpace(mesh, "DG", 1)
+    R = FunctionSpace(mesh, "R", 0)
+
+    # Function to store the solutions:
+    z = Function(Z)  # a field over the mixed function space Z.
+    u, p = split(z)  # Returns symbolic UFL expression for u and p
+    u_, p_ = z.subfunctions  # Returns individual Function for output
+    # Next rename for output:
+    u_.rename("Incremental Displacement")
+    p_.rename("Pressure")
+
+    displacement = Function(V, name="displacement")
+    deviatoric_stress = Function(TP1, name='deviatoric_stress')
+
+    # Output function space information:
+    log("Number of Velocity DOF:", V.dim())
+    log("Number of Pressure DOF:", W.dim())
+    log("Number of Velocity and Pressure DOF:", V.dim()+W.dim())
+    log("Number of Temperature DOF:", Q.dim())
+
+    # timestepping
+    rho0 = Function(R).assign(Constant(4500))  # density in kg/m^3
+    g = 10  # gravitational acceleration in m/s^2
+    viscosity = Constant(1e21)  # Viscosity Pa s
+    shear_modulus = Constant(1e11)  # Shear modulus in Pa
+    maxwell_time = viscosity / shear_modulus
+
+    # Set up surface load
+    lam = D / 8  # wavelength of load in m
+    kk = 2 * pi / lam  # wavenumber in m^-1
+    F0 = Constant(1000)  # initial free surface amplitude in m
+    X = SpatialCoordinate(mesh)
+    eta = F0 * (1-cos(kk * X[0]))
+
+    # Timestepping parameters
+    year_in_seconds = 3600*24*365
+    tau0 = Constant(2 * kk * viscosity / (rho0 * g))
+    log("tau0 in years", float(tau0/year_in_seconds))
+    time = Constant(0.0)
+    dt = Constant(dt_factor * tau0)  # Initial time-step
+    max_timesteps = round(2*tau0/dt)
+    log("max timesteps", max_timesteps)
+    dump_period = 1
+    log("dump_period", dump_period)
+    log("dt in years", float(dt/year_in_seconds))
+    log("maxwell time in years", float(maxwell_time/year_in_seconds))
+
+    approximation = SmallDisplacementViscoelasticApproximation(rho0, displacement, g=g)
+
+    # Create output file
+    if OUTPUT:
+        output_file = VTKFile(f"{output_directory}viscoelastic_freesurface_maxwelltime{float(maxwell_time/year_in_seconds):.0f}a_nx{nx}_dt{float(dt/year_in_seconds):.0f}a_tau{float(tau0/year_in_seconds):.0f}.pvd")
+
+    # Setup boundary conditions
+    stokes_bcs = {
+        bottom_id: {'uy': 0},
+        top_id: {'normal_stress': rho0*g*eta, 'free_surface': {"delta_rho_fs": rho0}},
+        left_id: {'ux': 0},
+        right_id: {'ux': 0},
+    }
+
+    # Setup analytical solution for the free surface from Cathles et al. 2024
+    eta_analytical = Function(Q, name="eta analytical")
+    h_elastic = Constant((F0*rho0*g/(2*kk*shear_modulus))/(1+maxwell_time/tau0))
+    log("Maximum initial elastic displacement:", float(h_elastic))
+    eta_analytical.interpolate(((F0 - h_elastic) * (1-exp(-(time)/(tau0+maxwell_time)))+h_elastic) * cos(kk * X[0]))
+    error = 0  # Initialise error
+
+    stokes_solver = ViscoelasticStokesSolver(z, viscosity, shear_modulus, rho0,
+                                             deviatoric_stress, displacement, approximation,
+                                             dt, bcs=stokes_bcs,)
+
+    if OUTPUT:
+        output_file.write(u_, displacement, p_, eta_analytical, deviatoric_stress, stokes_solver.previous_stress)
+
+    # Now perform the time loop:
+    for timestep in range(1, max_timesteps+1):
+
+        # Solve stokes system
+        stokes_solver.solve()
+        time.assign(time+dt)
+
+        # Update analytical solution
+        eta_analytical.interpolate(((F0 - h_elastic) * (1-exp(-(time)/(tau0+maxwell_time)))+h_elastic) * cos(kk * X[0]))
+
+        # Calculate error
+        local_error = assemble(pow(displacement[1]-eta_analytical, 2)*ds(top_id))
+        error += local_error * float(dt)
+
+        # Write output:
+        if timestep % dump_period == 0:
+            log("timestep", timestep)
+            log("time", float(time))
+            if OUTPUT:
+                output_file.write(u_, displacement, p_, eta_analytical, deviatoric_stress, stokes_solver.previous_stress)
+
+    final_error = pow(error, 0.5)/L
+    return final_error
+
+
+# Run default case run for four dt factors
+dtf_start = 0.1  # First dt is 1/10th of viscous relaxation time, by fourth refinement dt < maxwell time
+dt_factors = dtf_start / (2 ** np.arange(4))
+nx = 80
+
+prefix = "errors-viscoelastic-zhong"
+errors = np.array([viscoelastic_model(nx, dtf) for dtf in dt_factors])
+np.savetxt(f"{prefix}-free-surface.dat", errors)
