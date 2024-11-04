c504ec453ee1991686092755e79299c1cf63dcb9
Angus Gibson <angus@agibson.me>
2024-11-05 09:26:03 +1100

    Un-skip adjoint_2d_cylindrical

--- demos/adjoint_2d_cylindrical/test_cylindrical_adjoint.py
+++ demos/adjoint_2d_cylindrical/test_cylindrical_adjoint.py
@@ -4,7 +4,6 @@ from cases import cases
 
 
 @pytest.mark.parametrize("case_name", cases)
-@pytest.mark.skip(reason="currently untested")
 def test_annulus_taylor_test(case_name):
     with open(Path(__file__).parent.resolve() / f"{case_name}.conv", "r") as f:
         minconv = float(f.read())
