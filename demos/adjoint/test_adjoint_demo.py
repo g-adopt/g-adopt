696a3137f2452251d4e3b3fd558f7615f4b3b55e
Rhodri Davies <rhodri.davies@anu.edu.au>
2024-11-06 14:55:03 +1100

    Update test tolerance, now that smoothing is working correctly and the correct layer average is set.

--- demos/adjoint/test_adjoint_demo.py
+++ demos/adjoint/test_adjoint_demo.py
@@ -5,4 +5,4 @@ def test_adjoint_optimisation():
     with open(Path(__file__).parent.resolve() / "functional.txt", "r") as f:
         functional_values = [float(x) for x in f.readlines()]
 
-    assert functional_values[-1] < 1e-5
+    assert functional_values[-1] < 2e-4
