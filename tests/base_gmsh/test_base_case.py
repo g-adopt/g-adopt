29db6cf323e6f544c27be27a773669df7ec7172d
Dale Roberts <dale.roberts1@anu.edu.au>
2024-12-03 13:12:54 +1100

    Absolute paths to test output

--- tests/base_gmsh/test_base_case.py
+++ tests/base_gmsh/test_base_case.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from pathlib import Path
 
 
 # Minimal G-ADOPT example test
 def test_base_case():
-    df = pd.read_csv("params.log", sep="\\s+", header=0).iloc[-1]
-    expected = pd.read_pickle("expected.pkl")
+    b = Path(__file__).parent.resolve()
+    df = pd.read_csv(b / "params.log", sep="\\s+", header=0).iloc[-1]
+    expected = pd.read_pickle(b / "expected.pkl")
     pd.testing.assert_series_equal(df[["u_rms", "nu_top"]], expected, check_names=False)
     assert abs(df.name - expected.name) <= 2
