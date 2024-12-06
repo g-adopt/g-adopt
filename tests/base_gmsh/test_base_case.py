af1d5685b65e121e5fef664f899b2ee3a0ae5567
Dale Roberts <dale.roberts1@anu.edu.au>
2024-12-06 13:26:08 +1100

    Address review comments

--- tests/base_gmsh/test_base_case.py
+++ tests/base_gmsh/test_base_case.py
@@ -23,7 +23,6 @@ else:
 
 @pytest.mark.xfail(mesh_md5 != expected_md5, reason=warn_str)
 def test_base_case():
-    b = Path(__file__).parent.resolve()
     df = pd.read_csv(b / "params.log", sep="\\s+", header=0).iloc[-1]
     expected = pd.read_pickle(b / "expected.pkl")
     kwargs = {"check_names": False}
