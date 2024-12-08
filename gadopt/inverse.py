3279faf578b018e8f34bb2a20e0bada0f9059c8b
Angus Gibson <angus@agibson.me>
2024-12-09 10:53:14 +1100

    Generate optimisation checkpoints with collective ID (#151)
    Mistaken change in 30a3388 (Use comm-free UUID for optimisation
    checkpointing) broke parallel compatibility of optimisation
    checkpointing. Of course, the checkpoints for the ROL vectors need to be
    collective, so they all need to write to the same file with the same ID.
    We can go back to Firedrake's _get_uid, and just use the communicator
    associated with the function on the vector.

--- gadopt/inverse.py
+++ gadopt/inverse.py
@@ -5,8 +5,8 @@ relevant parameters and call the `run` method to perform the optimisation.
 """
 
 from pathlib import Path
-import uuid
 
+import firedrake.utils
 import pyadjoint.optimization.rol_solver as pyadjoint_rol
 import ROL
 from firedrake import CheckpointFile, Function
@@ -246,6 +246,7 @@ class CheckpointedROLVector(pyadjoint_rol.ROLVector):
         """
         super().__init__(dat, inner_product)
 
+        self.comm = dat[0].comm
         self._optimiser = optimiser
 
     def clone(self):
@@ -297,7 +298,8 @@ class CheckpointedROLVector(pyadjoint_rol.ROLVector):
     def __getstate__(self):
         """Returns a state tuple suitable for pickling"""
 
-        checkpoint_filename = f"vector_checkpoint_{uuid.uuid4()}.h5"
+        checkpoint_id = firedrake.utils._new_uid(self.comm)
+        checkpoint_filename = f"vector_checkpoint_{checkpoint_id}.h5"
         checkpoint_path = self._optimiser.checkpoint_dir / checkpoint_filename
         self.save(checkpoint_path)
 
--- tests/optimisation_checkpointing/test_checkpointing.py
+++ tests/optimisation_checkpointing/test_checkpointing.py
@@ -1,17 +1,19 @@
 import numpy as np
+import pytest
 from pathlib import Path
 
 
-def test_checkpointing():
+@pytest.mark.parametrize("procs", [1, 2])
+def test_checkpointing(procs):
     base = Path(__file__).parent.resolve()
 
-    full_optimisation = np.loadtxt(base / "full_optimisation.dat")
-    restored_optimisation = np.loadtxt(base / "restored_optimisation_from_it_5.dat")
+    full_optimisation = np.loadtxt(base / f"full_optimisation_np{procs}.dat")
+    restored_optimisation = np.loadtxt(base / f"restored_optimisation_from_it_5_np{procs}.dat")
 
     restored_steps = restored_optimisation.size
 
     assert np.allclose(full_optimisation[-restored_steps:], restored_optimisation)
 
-    restored_optimisation_last_it = np.loadtxt(base / "restored_optimisation_from_last_it.dat")
+    restored_optimisation_last_it = np.loadtxt(base / f"restored_optimisation_from_last_it_np{procs}.dat")
 
     assert full_optimisation[-1] > restored_optimisation_last_it[0]
