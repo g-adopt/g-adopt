ddc55316cdaf127417c207540651e1b25be78419
w3168 <wis15@ic.ac.uk>
2024-12-12 14:09:05 +1100

    Remove print statments

--- tests/scalar_advection/test_scalar_advection.py
+++ tests/scalar_advection/test_scalar_advection.py
@@ -8,8 +8,6 @@ base = Path(__file__).parent.resolve()
 def test_scalar_advection():
     expected_error = np.load(base / "expected_error.npy")
     final_error = np.loadtxt(base / "final_error.log")
-    print(expected_error)
-    print(final_error)
 
     # check that norm(q) is the same as previously run
     assert np.allclose(final_error, expected_error, rtol=1e-6, atol=1e-16)
