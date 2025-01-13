2d91631ac302531339c558ee71aba0b59f5dab71
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-01-14 10:38:01 +1100

    make mesh spherical

--- demos/adjoint_spherical/adjoint.py
+++ demos/adjoint_spherical/adjoint.py
@@ -121,6 +121,8 @@ def forward_problem():
         # Average temperature field
         Tave = fi.load_function(mesh, name="AverageTemperature")
 
+    mesh.cartesian = True
+
     # Boundary markers to top and bottom
     bottom_id, top_id = "bottom", "top"
 
