8d5a714aa15afe739f6b5f315c521946d1934d5d
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-01-14 15:57:38 +1100

    Adding GC stuff

--- demos/smoothing/smoothing_gadi.py
+++ demos/smoothing/smoothing_gadi.py
@@ -14,13 +14,17 @@ def __main__():
         mesh = f.load_mesh("firedrake_default_extruded")
         T_simulation = f.load_function(mesh, name="Temperature")
 
+    Q_CG = FunctionSpace(mesh, "CG", 1)
+    T_simulation_GC = Function(Q_CG, name="Temperature")
+    T_simulation_GC.interpolate(T_simulation)
+
     temp_bcs = {
         "bottom": {'T': 1.0 - 930. / 3700},
         "top": {'T': 0.0},
     }
 
     # Projection solver parameters for nullspaces:
-    iterative_energy_solver_parameters: dict[str, Any] = {
+    iterative_energy_solver_parameters = {
         "mat_type": "aij",
         "snes_type": "ksponly",
         "ksp_type": "gmres",
@@ -28,18 +32,16 @@ def __main__():
         "pc_type": "sor",
     }
 
-    import pdb
-    pdb.set_trace()
     # Adding Smoothing to Tobs
     smoother = DiffusiveSmoothingSolver(
-        function_space=T_simulation.function_space(),
+        function_space=T_simulation_GC.function_space(),
         wavelength=0.05,
         bcs=temp_bcs,
         solver_parameters=iterative_energy_solver_parameters,
     )
 
     # acting smoothing on Tobs
-    T_smooth = smoother.action(T_simulation)
+    T_smooth = smoother.action(T_simulation_GC)
 
     output_path = base_path / "smoothed_temperature.h5"
 
