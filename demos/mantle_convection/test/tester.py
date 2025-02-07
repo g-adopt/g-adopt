119f566adc53aeac7455b9ec90985f5276add2a5
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-02-07 15:18:55 +1100

    dropping w

--- demos/mantle_convection/test/tester.py
+++ demos/mantle_convection/test/tester.py
@@ -22,23 +22,14 @@ def rf_generator():
     tape.clear_tape()
     continue_annotation()
     enable_disk_checkpointing()
-    x_max = 1.0
-    y_max = 1.0
-    disc_n = 150
-
-    mesh1d = IntervalMesh(disc_n, length_or_left=0.0, right=x_max)
-    mesh = ExtrudedMesh(
-        mesh1d, layers=disc_n, layer_height=y_max / disc_n, extrusion_type="uniform"
-    )
+    mesh = RectangleMesh(100, 100, 1.0, 1.0)
     mesh = checkpointable_mesh(mesh)
 
-    V = VectorFunctionSpace(mesh, "CG", 2)
     Q = FunctionSpace(mesh, "CG", 1)
 
     # Define the rotation vector field
     X = SpatialCoordinate(mesh)
 
-    w = Function(V, name="rotation").interpolate(as_vector([-X[1] - 0.5, X[0] - 0.5]))
     T_c = Function(Q, name="control")
     T = Function(Q, name="Temperature")
     T_c.interpolate(0.1 * exp(-0.5 * ((X - as_vector((0.75, 0.5))) / Constant(0.1)) ** 2))
