0f829416420622f63bf644f3c642fa953889f388
Angus Gibson <angus@agibson.me>
2025-02-28 16:58:49 +1100

    Convert tests directory to new run structure
    Had to define a canned recipe for running our Python wrapper, and allow
    it to accept arguments. The analytical comparison is still a bit funky,
    and I've decided to just assume you're using tsp.

--- rules.mk
+++ rules.mk
@@ -5,16 +5,19 @@ run_regular = d=$$(date +%s); $(1) && echo "$(2) took $$(($$(date +%s)-d)) secon
 run_batch = id=$$(tsp -f $(if $(filter-out 1,$(ncpus)),-N $(ncpus)) $(if $(category),-L $(category)) $(1)) && echo "[$(category)]$(2) took $$(tsp -i $$id | sed -n 's/Time run: //p')"
 run_cmd = $(if $(BATCH_MODE),$(run_batch),$(run_regular))
 
-exec_cmd = $(if $(filter-out 1,$(ncpus)),mpiexec -np $(ncpus)) python3 $< $(PETSC_FLAGS)
+exec_cmd = $(if $(filter-out 1,$(ncpus)),mpiexec -np $(ncpus)) python3 $< $(PETSC_FLAGS) $(exec_args)
 
 # running in batch mode (i.e. from the CI) we want to set a timeout
 # so that we can get useful output without hitting the GitHub Actions
 # hard limit of 6 hours
 exec_wrapper = $(if $(BATCH_MODE),timeout 2h $(exec_cmd),$(exec_cmd))
 
-run-%: %.py
-	@$(call run_cmd,$(exec_wrapper),$(or $(desc),$<))
+define run-python =
+@$(call run_cmd,$(exec_wrapper),$(or $(desc),$<))
+endef
 
+run-%: %.py
+	$(run-python)
 
 # define these as expansion rules so they can be used to refer to the calling case
 current_dir = $(notdir $(patsubst %/,%,$(CURDIR)))
