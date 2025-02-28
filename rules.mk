cff12258412f53921b86b9dee578bcd6e6223970
Angus Gibson <angus@agibson.me>
2025-02-28 14:26:01 +1100

    Add some timing information to runs
    It would be nice to print to stdout when tsp starts a queued job, but
    that requires cooperation from tsp itself, which it doesn't support.

--- rules.mk
+++ rules.mk
@@ -1,11 +1,19 @@
-# if we're running from the CI, we want to run everything
-# with tsp, echo the job on start and finish, and
-# set a timeout
+# running in batch mode, we defer scheduling to tsp
+# otherwise, we just run directly
+# in both cases, we print some timing information
+run_regular = d=$$(date +%s); $(1) && echo "$(2) took $$(($$(date +%s)-d)) seconds"
+run_batch = id=$$(tsp -f $(if $(category),-L $(category)) $(1)) && echo "[$(category)]$(2) took $$(tsp -i $$id | sed -n 's/Time run: //p')"
+run_cmd = $(if $(BATCH_MODE),$(run_batch),$(run_regular))
 
-RUN_CMD = $(if $(BATCH_MODE),tsp $(if $(category),-L $(category),))
+exec_cmd = $(if $(filter-out 1,$(ncpus)),mpiexec -np $(ncpus)) python3 $< $(PETSC_FLAGS)
+
+# running in batch mode (i.e. from the CI) we want to set a timeout
+# so that we can get useful output without hitting the GitHub Actions
+# hard limit of 6 hours
+exec_wrapper = $(if $(BATCH_MODE),timeout 2h $(exec_cmd),$(exec_cmd))
 
 run-%: %.py
-	$(RUN_CMD) $(if $(filter-out 1,$(ncpus)),mpiexec -np $(ncpus)) python3 $< $(PETSC_FLAGS)
+	@$(call run_cmd,$(exec_wrapper),$(or $(desc),$<))
 
 
 # define these as expansion rules so they can be used to refer to the calling case
