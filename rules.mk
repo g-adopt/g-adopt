b75fdda699df258c94d01b53397a234988fa9a55
Angus Gibson <angus@agibson.me>
2025-02-28 15:51:42 +1100

    Tell tsp how many slots a job should take

--- rules.mk
+++ rules.mk
@@ -2,7 +2,7 @@
 # otherwise, we just run directly
 # in both cases, we print some timing information
 run_regular = d=$$(date +%s); $(1) && echo "$(2) took $$(($$(date +%s)-d)) seconds"
-run_batch = id=$$(tsp -f $(if $(category),-L $(category)) $(1)) && echo "[$(category)]$(2) took $$(tsp -i $$id | sed -n 's/Time run: //p')"
+run_batch = id=$$(tsp -f $(if $(filter-out 1,$(ncpus)),-N $(ncpus)) $(if $(category),-L $(category)) $(1)) && echo "[$(category)]$(2) took $$(tsp -i $$id | sed -n 's/Time run: //p')"
 run_cmd = $(if $(BATCH_MODE),$(run_batch),$(run_regular))
 
 exec_cmd = $(if $(filter-out 1,$(ncpus)),mpiexec -np $(ncpus)) python3 $< $(PETSC_FLAGS)
