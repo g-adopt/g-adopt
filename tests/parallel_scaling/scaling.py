802eb0694839a25d00779763e52806e4e5624ce5
Dale Roberts <dale.roberts1@anu.edu.au>
2025-03-18 08:25:40 +0800

    [skip tests] Prototype multi-system extension for parallel_scaling tests

--- tests/parallel_scaling/scaling.py
+++ tests/parallel_scaling/scaling.py
@@ -1,8 +1,10 @@
 import argparse
 import numpy as np
+import os
 import re
 import subprocess
 import sys
+import socket
 from pathlib import Path
 
 cases = {
@@ -27,6 +29,47 @@ cases = {
         "timestep": 6.25e-9,
     },
 }
+
+
+class HPCDetails:
+    batch_templates = {
+        "gadi": "qsub -v GADOPT_CHECKOUT={gadopt_checkout},GADOPT_SETUP={gadopt_setup} -W block=true -N scaling_{level} -l storage=gdata/xd2+scratch/xd2+gdata/fp50,ncpus={cores},walltime=04:00:00,mem={mem}GB,wd,jobfs=200GB -q normalsr -P {project} -o pbs_output/l{level}.out -e pbs_output/l{level}.err -- ./run_gadi.sh {level}",
+        "setonix": "sbatch --export GADOPT_CHECKOUT={gadopt_checkout},GADOPT_SETUP={gadopt_setup} --wait -J scaling_{level} --exclusive --ntasks={cores} --nodes={nodes} -t 4:00:00 -p work -A {project} -o slurm_output/l{level}.out -o slurm_output/l{level}.err -- ./run_setonix.sh {level}",
+    }
+    required_environment = {
+        "gadi": {"gadopt_checkout", "gadopt_setup", "project"},
+        "setonix": {"gadopt_checkout", "gadopt_setup", "project"},
+    }
+
+    @staticmethod
+    def hpc_system_is_gadi():
+        return socket.gethostname().startswith("gadi")
+
+    @staticmethod
+    def hpc_system_is_setonix():
+        if os.path.isfile("/software/pawsey/motd"):
+            with open("/software/pawsey/motd") as f:
+                for line in f.readlines():
+                    if line == "     ___  ___| |_ ___  _ __ (_)_  __           ,########(,,  ...     ,########/,\n":
+                        return True
+            return False
+        else:
+            return False
+
+
+def get_hpc_template():
+    system = None
+    for name in dir(HPCDetails):
+        if name.startswith("hpc_system_is"):
+            if getattr(HPCDetails, name)():
+                system = name.split("_")[-1]
+    if not system:
+        raise KeyError("HPC system requested but could not identify system")
+    # check environment variables
+    for var in HPCDetails.required_environment[system]:
+        if var not in os.environ:
+            raise KeyError(f"{var} is required in environment when running on {system}")
+    return HPCDetails.batch_templates[system], {var: os.environ[var] for var in HPCDetails.required_environment[system]}
 
 
 def get_data(level, base_path=None):
@@ -93,12 +136,17 @@ def run_subcommand(args):
 def submit_subcommand(args):
     config = cases[args.level]
     cores = config.pop("cores")
-    command = args.template.format(cores=cores, mem=4*cores, level=args.level)
-
+    command = args.template.format(
+        cores=cores, mem=4*cores, level=args.level, nodes=max(1, cores//104), **args.extra_format
+    )
     proc = subprocess.Popen(
         [
-            *command.split(), sys.executable, sys.argv[0],
-            "run", str(args.level), *[str(v) for v in config.values()],
+            *command.split(),
+            os.path.basename(sys.executable),
+            sys.argv[0],
+            "run",
+            str(args.level),
+            *[str(v) for v in config.values()],
         ],
     )
     if proc.wait() != 0:
@@ -112,17 +160,29 @@ if __name__ == "__main__":
         description="Run/submit parallel scaling test casse",
     )
     subparsers = parser.add_subparsers(title="subcommands")
-
-    parser_run = subparsers.add_parser("run", help="run a specific configuration of a case (usually not manually invoked)")
+    parser_run = subparsers.add_parser(
+        "run", help="run a specific configuration of a case (usually not manually invoked)"
+    )
     parser_run.add_argument("level", type=int)
     parser_run.add_argument("layers", type=int)
     parser_run.add_argument("timestep", type=float)
     parser_run.add_argument("-n", "--steps", type=int, help="number of timesteps to run")
     parser_run.set_defaults(func=run_subcommand)
     parser_submit = subparsers.add_parser("submit", help="submit a PBS job to run a specific case")
-    parser_submit.add_argument("-t", "--template", default="mpiexec -np {cores}", help="template command for running commands under MPI")
+    group = parser_submit.add_mutually_exclusive_group()
+    group.add_argument(
+        "-t", "--template", default="mpiexec -np {cores}", help="template command for running commands under MPI"
+    )
+    group.add_argument(
+        "-H",
+        "--HPC",
+        help="Detect HPC system and run using known template for batch job submission for that system",
+        action="store_true",
+    )
     parser_submit.add_argument("level", type=int)
     parser_submit.set_defaults(func=submit_subcommand)
 
     args = parser.parse_args()
+    if hasattr(args, "HPC") and args.HPC:
+        args.template, args.extra_format = get_hpc_template()
     args.func(args)
