a711db046d7027ed076a0fe919daf07a13af65d6
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-04-09 20:07:21 +1000

    cases.py now includes the different schedulers

--- tests/adjoint_2d_cylindrical/cases.py
+++ tests/adjoint_2d_cylindrical/cases.py
@@ -1,6 +1,13 @@
+from checkpoint_schedules import SingleMemoryStorageSchedule, SingleDiskStorageSchedule
+
 cases = {
     "damping": [-1, -1, +1, -1],
     "smoothing": [-1, -1, -1, +1],
     "Tobs": [+1, -1, -1, -1],
     "uobs": [-1, +1, -1, -1]
+}
+
+schedulers = {
+    "fullmemory": SingleMemoryStorageSchedule(),
+    "fulldisk": SingleDiskStorageSchedule(),
 }
