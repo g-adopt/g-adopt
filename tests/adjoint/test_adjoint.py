e1b79372d37d6e96f4d97826d3791badbc166379
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-04-23 09:31:52 +0330

    schedules is a dictionary

--- tests/adjoint/test_adjoint.py
+++ tests/adjoint/test_adjoint.py
@@ -4,7 +4,7 @@ from cases import cases, schedules
 
 
 @pytest.mark.parametrize("case_name", cases)
-@pytest.mark.parametrize("schedule_name", schedules)
+@pytest.mark.parametrize("schedule_name", schedules.keys())
 def test_rectangular_taylor_test(case_name, schedule_name):
     case_scheduler = f"{case_name}_{schedule_name}"
     with open(Path(__file__).parent.resolve() / f"{case_scheduler}.conv", "r") as f:
