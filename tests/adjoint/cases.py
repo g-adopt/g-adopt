397bca8ccfb70d831363a6cbaf452af375b26632
Sia Ghelichkhan <siavash.ghelichkhan@anu.edu.au>
2025-06-11 16:42:27 +1000

    Tests for adjoint with changing Dirichlet boundary condition (#222)
    Adding the uimposed case to our regressions tests (checking if DirichletBCs are being checked)

--- tests/adjoint/cases.py
+++ tests/adjoint/cases.py
@@ -1,4 +1,14 @@
-from checkpoint_schedules import SingleMemoryStorageSchedule
+from checkpoint_schedules import SingleMemoryStorageSchedule, SingleDiskStorageSchedule
 
-cases = ["damping", "smoothing", "Tobs", "uobs"]
-schedules = {"fullmemory": SingleMemoryStorageSchedule()}
+cases = [
+    "damping",  # Taylor test on the damping term only  $$ (Tic - T_ave) ** 2 dx $$
+    "smoothing",  # Taylor test on smoothing only  $$ \nabla (T_ic - T_ave) ** 2 dx$$
+    "Tobs",  # Taylor test on temperature misfit only  $$ (T - T_obs) ** 2 dx $$
+    "uobs",  # Taylor test on uobs only $$ (u - uobs) ** 2 ds_t $$
+    "uimposed",  # Taylor test on temperature misfit, but with imposed velocity $$ (T - T_obs) ** 2 dx $$
+]
+schedules = {
+    "noscheduler": None,  # No scheduler used
+    "fullmemory": SingleMemoryStorageSchedule(),  # Store all data in memory
+    "fullstorage": SingleDiskStorageSchedule(),  # Store all data on disk
+}
