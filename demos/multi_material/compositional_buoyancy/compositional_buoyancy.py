7de45be2f11e6372ed918591ae022688c989fdd6
Thomas Duvernay <td75013@hotmail.fr>
2025-08-04 12:42:36 +1000

    Update multi-material demos and tests

--- demos/multi_material/compositional_buoyancy/compositional_buoyancy.py
+++ demos/multi_material/compositional_buoyancy/compositional_buoyancy.py
@@ -1,59 +1,61 @@
-# Rayleigh-Taylor instability
-# ===========================
-#
+# # Rayleigh-Taylor instability
+# ---
+
 # Rationale
-# ---------
-#
+# -
+
 # One may wish to simulate a geodynamical flow involving multiple physical phases. A
-# possible approach is to approximate phases as immiscible and forming a single fluid
-# whose dynamics can still be described as a single-phase Stokes flow. Under this
-# approximation, it is common to refer to immiscible phases as materials and to the
-# resulting simulations as multi-material. In such simulations, each material occupies
-# part of the numerical domain and is characterised by its own physical properties,
-# such as density and viscosity. Along material boundaries, physical properties are
-# averaged according to a chosen mathematical scheme.
-#
+# possible approach is to approximate phases as immiscible, forming a single fluid that
+# still behaves as a single-phase Stokes flow. Under this approximation, it is usual to
+# refer to immiscible phases as materials and the resulting simulations as
+# multi-material. In such simulations, each material occupies part of the numerical
+# domain and has intrinsic physical properties, such as density and viscosity. Across a
+# material interface, physical properties transition from one value to another, either
+# sharply or smoothly. In the former case, physical property fields exhibit
+# discontinuities, whilst in the latter case, an averaging scheme weights contributions
+# from each material.
+
 # Numerical approach
-# ------------------
-#
-# To model the coexistence of multiple materials in the numerical domain, we employ an
-# interface-capturing approach called the conservative level-set method. Level-set
-# methods associate each material interface to a mathematical field representing a
-# measure of distance from that interface. In the conservative level-set approach, the
-# classic signed-distance function, $\phi$, employed in the level-set method is
-# transformed into a smooth step function, $\psi$, according to
-#
+# -
+
+# We employ an interface-capturing approach called the conservative level-set method
+# ([Olsson and Kreiss, 2005](https://doi.org/10.1016/j.jcp.2005.04.007)) to model the
+# coexistence of multiple materials in the numerical domain. Level-set methods associate
+# each material interface to a mathematical field measuring the distance from that
+# interface. In the conservative level-set approach, the classic signed-distance
+# function, $\phi$, employed in the level-set method is transformed into a smooth step
+# function, $\psi$:
+
 # $$\psi(\mathbf{x}, t) = \frac{1}{2} \left[
 # \mathrm{tanh} \left( \frac{\phi(\mathbf{x}, t)}{2\epsilon} \right) + 1
 # \right]$$
-#
+
 # Throughout the simulation, the level-set field is advected with the flow:
-#
+
 # $$\frac{\partial \psi}{\partial t} + \nabla \cdot \left( \mathbf{u}\psi \right) = 0$$
-#
+
 # Advection of the level set modifies the shape of the initial profile. In other words,
 # the signed-distance property underpinning the smooth step function is lost. To
-# maintain the original profile as the simulation proceeds, a reinitialisation
+# maintain the original profile whilst the simulation proceeds, a reinitialisation
 # procedure is employed. We choose the equation proposed in [Parameswaran and Mandal
-# (2023)](https://www.sciencedirect.com/science/article/pii/S0997754622001364):
-#
+# (2023)](https://doi.org/10.1016/j.euromechflu.2022.11.001):
+
 # $$\frac{\partial \psi}{\partial \tau_{n}} = \theta \left[
 # -\psi \left( 1 - \psi \right) \left( 1 - 2\psi \right)
 # + \epsilon \left( 1 - 2\psi \right) \lvert\nabla\psi\rvert
 # \right]$$
-#
-# This example
-# ------------
-#
-# Here, we consider the isoviscous Rayleigh-Taylor instability presented in [van Keken
-# et al. (1997)](https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/97JB01353).
-# Inside a 2-D domain, a buoyant, lighter material sits beneath a denser material. The
-# initial material interface promotes the development of a rising instability on the
-# domain's left-hand side, and further smaller-scale convective dynamics take place
-# throughout the remainder of the simulation. We describe below how to implement this
-# problem using G-ADOPT.
 
-# As with all examples, the first step is to import the `gadopt` package, which
+# This example
+# -
+
+# Here, we consider the isoviscous Rayleigh-Taylor instability presented in [van Keken
+# et al. (1997)](https://doi.org/10.1029/97JB01353). Inside a 2-D domain, a buoyant,
+# lighter material sits beneath a denser material. The initial material interface
+# promotes the development of a rising instability on the domain's left-hand side, and
+# further convective dynamics occur throughout the remainder of the simulation. We
+# describe below the implementation of this problem using G-ADOPT.
+
+# As with all examples, the first step is to import the `gadopt` package, which also
 # provides access to Firedrake and associated functionality.
 
 from gadopt import *
@@ -62,24 +64,24 @@ from gadopt import *
 # solutions, as in our previous tutorials.
 
 # +
-nx, ny = 40, 40  # Number of cells in x and y directions
-lx, ly = 0.9142, 1  # Domain dimensions in x and y directions
+mesh_elements = (64, 64)  # Number of cells in x and y directions
+domain_dims = (0.9142, 1.0)  # Domain dimensions in x and y directions
 # Rectangle mesh generated via Firedrake
-mesh = RectangleMesh(nx, ny, lx, ly, quadrilateral=True)
-mesh.cartesian = True
-boundary = get_boundary_ids(mesh)
+mesh = RectangleMesh(*mesh_elements, *domain_dims, quadrilateral=True)
+mesh.cartesian = True  # Tag the mesh as Cartesian to inform other G-ADOPT objects
+boundary = get_boundary_ids(mesh)  # Object holding references to mesh boundary IDs
 
-V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
-W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+V = VectorFunctionSpace(mesh, "Q", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "Q", 1)  # Pressure function space (scalar)
 Z = MixedFunctionSpace([V, W])  # Stokes function space (mixed)
-Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+Q = FunctionSpace(mesh, "DQ", 2)  # Temperature function space (scalar)
 K = FunctionSpace(mesh, "DQ", 2)  # Level-set function space (scalar, discontinuous)
-R = FunctionSpace(mesh, "R", 0)  # Real space for time step
+R = FunctionSpace(mesh, "R", 0)  # Real space (constants across the domain)
 
-z = Function(Z)  # A field over the mixed function space Z
-u, p = split(z)  # Symbolic UFL expressions for velocity and pressure
-z.subfunctions[0].rename("Velocity")  # Associated Firedrake velocity function
-z.subfunctions[1].rename("Pressure")  # Associated Firedrake pressure function
+stokes = Function(Z)  # A field over the mixed function space Z
+stokes.subfunctions[0].rename("Velocity")  # Firedrake function for velocity
+stokes.subfunctions[1].rename("Pressure")  # Firedrake function for pressure
+u = split(stokes)[0]  # Indexed expression for velocity in the mixed space
 T = Function(Q, name="Temperature")  # Firedrake function for temperature
 psi = Function(K, name="Level set")  # Firedrake function for level set
 # -
@@ -105,12 +107,12 @@ from numpy import linspace  # noqa: E402
 # G-ADOPT API to generate the thickness of the hyperbolic tangent profile and update the
 # level-set field values.
 callable_args = (
-    curve_parameter := linspace(0, lx, 1000),
+    curve_parameter := linspace(0.0, domain_dims[0], 1000),
     interface_deflection := 0.02,
-    perturbation_wavelength := 2 * lx,
+    perturbation_wavelength := 2.0 * domain_dims[0],
     interface_coord_y := 0.2,
 )
-boundary_coordinates = [(lx, ly), (0.0, ly), (0.0, interface_coord_y)]
+boundary_coordinates = [domain_dims, (0.0, domain_dims[1]), (0.0, interface_coord_y)]
 
 epsilon = interface_thickness(K)
 assign_level_set_values(
@@ -131,25 +133,21 @@ assign_level_set_values(
 
 # fig, axes = plt.subplots()
 # axes.set_aspect("equal")
-# contours = tricontourf(psi, levels=linspace(0, 1, 11), axes=axes, cmap="PiYG")
+# contours = tricontourf(psi, levels=linspace(0.0, 1.0, 11), axes=axes, cmap="PiYG")
 # tricontour(psi, axes=axes, levels=[0.5])
-# fig.colorbar(contours, label="Conservative level-set")
+# fig.colorbar(contours, label="Conservative level set")
 # -
 
-# We next define materials present in the simulation using the `Material` class. Here,
-# the problem is non-dimensionalised and can be described by the product of the
-# expressions for the Rayleigh and buoyancy numbers, RaB, which is also referred to as
-# compositional Rayleigh number. Therefore, we provide a value for thermal and
-# compositional Rayleigh numbers to define our approximation. Material fields, such as
-# RaB, are created using the `field_interface` function, which generates a unique field
-# over the numerical domain based on the level-set field(s) and values or expressions
-# associated with each material. At the interface between two materials, the transition
-# between values or expressions can be represented as sharp or diffuse, with the latter
-# using averaging schemes, such as arithmetic, geometric, and harmonic means.
-
+# We next define the material fields and instantiate the approximation. Here, the system
+# of equations is non-dimensional and only includes compositional buoyancy under the
+# Boussinesq approximation. Moreover, physical parameters are constant through space
+# apart from density. As a result, the system is fully defined by the compositional
+# Rayleigh number. We use the `material_field` function to define its value throughout
+# the domain (including the shape of the material interface transition) and provide it
+# to our approximation.
 
 # +
-Ra = 0  # Thermal Rayleigh number
+Ra = 0.0  # Thermal Rayleigh number
 # Compositional Rayleigh number, defined based on each material value and location
 RaB_buoyant = 0.0
 RaB_dense = 1.0
@@ -158,101 +156,106 @@ RaB = material_field(psi, [RaB_buoyant, RaB_dense], interface="arithmetic")
 approximation = BoussinesqApproximation(Ra, RaB=RaB)
 # -
 
+# Let us now verify that the material fields have been correctly initialised. We plot
+# the compositional Rayleigh number across the domain.
+
+# + tags=["active-ipynb"]
+# fig, axes = plt.subplots()
+# axes.set_aspect("equal")
+# contours = tricontourf(
+#     Function(psi).interpolate(Ra_c), levels=linspace(0.0, 1.0, 11), axes=axes
+# )
+# fig.colorbar(contours, label="Compositional Rayleigh number")
+# -
+
 # As with the previous examples, we set up an instance of the `TimestepAdaptor` class
 # for controlling the time-step length (via a CFL criterion) whilst the simulation
 # advances in time. We specify the initial time, initial time step $\Delta t$, and
 # output frequency (in time units).
 
-time_now = 0  # Initial time
-delta_t = Function(R).assign(1)  # Initial time step
-output_frequency = 10  # Frequency (based on simulation time) at which to output
+time_now = 0.0  # Initial time
+time_step = Function(R).assign(1.0)  # Initial time step
+output_frequency = 10.0  # Frequency (based on simulation time) at which to output
 t_adapt = TimestepAdaptor(
-    delta_t, u, V, target_cfl=0.6, maximum_timestep=output_frequency
+    time_step, u, V, target_cfl=0.6, maximum_timestep=output_frequency
 )  # Current level-set advection requires a CFL condition that should not exceed 0.6.
-
-# This problem setup has a constant pressure nullspace, which corresponds to the
-# default case handled in G-ADOPT.
-
-Z_nullspace = create_stokes_nullspace(Z)
-
-# Boundary conditions are specified next: no slip at the top and bottom and free slip
-# on the left and ride sides. No boundary conditions are required for level set, as the
-# numerical domain is closed.
-
-stokes_bcs = {
-    boundary.bottom: {"u": 0},
-    boundary.top: {"u": 0},
-    boundary.left: {"ux": 0},
-    boundary.right: {"ux": 0},
-}
-
-# We now set up our output. To do so, we create the output file as a ParaView Data file
-# that uses the XML-based VTK file format. We also open a file for logging, instantiate
-# G-ADOPT's geodynamical diagnostic utility, and define parameters to compute an
-# additional diagnostic specific to multi-material simulations, namely material
-# entrainment.
-
-# +
-output_file = VTKFile("output.pvd")
-
-plog = ParameterLog("params.log", mesh)
-plog.log_str("step time dt u_rms entrainment")
-
-gd = GeodynamicalDiagnostics(z, T, boundary.bottom, boundary.top)
-
-material_area = interface_coord_y * lx  # Area of tracked material in the domain
-entrainment_height = 0.2  # Height above which entrainment diagnostic is calculated
-# -
 
 # Here, we set up the variational problem for the Stokes and level-set systems. The
 # former depends on the approximation defined above, and the latter includes both
-# advection and reinitialisation components. Subcycling is available for level-set
-# advection and is mainly useful when the problem at hand involves multiple CFL
-# conditions, with the CFL for level-set advection being the most restrictive.
+# advection and reinitialisation components.
 
 # +
+# This problem setup has a constant pressure nullspace, which corresponds to the default
+# case handled in G-ADOPT.
+stokes_nullspace = create_stokes_nullspace(Z)
+
+# Boundary conditions for the Stokes system: no slip at the top and bottom and free slip
+# on the left and right sides.
+stokes_bcs = {
+    boundary.bottom: {"u": 0.0},
+    boundary.top: {"u": 0.0},
+    boundary.left: {"ux": 0.0},
+    boundary.right: {"ux": 0.0},
+}
+# Instantiate a solver object for the Stokes system and perform a solve to obtain
+# initial pressure and velocity.
 stokes_solver = StokesSolver(
-    z,
+    stokes,
     T,
     approximation,
     bcs=stokes_bcs,
-    nullspace=Z_nullspace,
-    transpose_nullspace=Z_nullspace,
+    nullspace=stokes_nullspace,
+    transpose_nullspace=stokes_nullspace,
 )
+stokes_solver.solve()
 
 # Instantiate a solver object for level-set advection and reinitialisation. G-ADOPT
 # provides default values for most arguments; we only provide those that do not have
 # one. No boundary conditions are required, as the numerical domain is closed.
-adv_kwargs = {"u": u, "timestep": delta_t}
+adv_kwargs = {"u": u, "timestep": time_step}
 reini_kwargs = {"epsilon": epsilon}
 level_set_solver = LevelSetSolver(psi, adv_kwargs=adv_kwargs, reini_kwargs=reini_kwargs)
 # -
 
-# Finally, we initiate the time loop, which runs until the simulation end time is
-# attained.
+# We now set up our output. To do so, we create the output file as a ParaView Data file
+# that uses the XML-based VTK file format. We also open a file for logging, instantiate
+# G-ADOPT geodynamical diagnostic utility, and define some parameters specific to this
+# problem.
 
 # +
-step = 0  # A counter to keep track of looping
-output_counter = 0  # A counter to keep track of outputting
-time_end = 2000
+output_file = VTKFile("output.pvd")
+output_file.write(*stokes.subfunctions, psi, time=time_now)
+
+plog = ParameterLog("params.log", mesh)
+plog.log_str("step time dt u_rms entrainment")
+
+gd = GeodynamicalDiagnostics(stokes, T, boundary.bottom, boundary.top)
+
+# Area of tracked material in the domain
+material_area = interface_coord_y * domain_dims[0]
+entrainment_height = 0.2  # Height above which entrainment diagnostic is calculated
+# -
+
+# Finally, we initiate the time loop, which runs until the simulation end time.
+
+# +
+step = 0  # A counter to keep track of loop iterations
+output_counter = 1  # A counter to keep track of outputting
+time_end = 2000.0
 while True:
-    # Write output
-    if time_now >= output_counter * output_frequency:
-        output_file.write(*z.subfunctions, T, psi)
-        output_counter += 1
-
     # Update timestep
-    if time_end is not None:
-        t_adapt.maximum_timestep = min(output_frequency, time_end - time_now)
+    if time_end - time_now < output_frequency:
+        t_adapt.maximum_timestep = time_end - time_now
     t_adapt.update_timestep()
-    time_now += float(delta_t)
-    step += 1
-
-    # Solve Stokes sytem
-    stokes_solver.solve()
 
     # Advect level set
     level_set_solver.solve()
+    # Solve Stokes sytem
+    stokes_solver.solve()
+
+    # Increment iteration count and time
+    step += 1
+    time_now += float(time_step)
 
     # Calculate proportion of material entrained above a given height
     buoy_entr = material_entrainment(
@@ -264,34 +267,34 @@ while True:
     )
 
     # Log diagnostics
-    plog.log_str(f"{step} {time_now} {float(delta_t)} {gd.u_rms()} {buoy_entr}")
+    plog.log_str(f"{step} {time_now} {float(time_step)} {gd.u_rms()} {buoy_entr}")
+
+    # Write output
+    if time_now >= output_counter * output_frequency:
+        output_file.write(*stokes.subfunctions, psi, time=time_now)
+        output_counter += 1
 
     # Check if simulation has completed
     if time_now >= time_end:
+        plog.close()  # Close logging file
+
+        # Checkpoint solution fields to disk
+        with CheckpointFile("Final_State.h5", "w") as final_checkpoint:
+            final_checkpoint.save_mesh(mesh)
+            final_checkpoint.save_function(stokes, name="Stokes")
+            final_checkpoint.save_function(psi, name="Level set")
+
         log("Reached end of simulation -- exiting time-step loop")
         break
 # -
 
-# At the end of the simulation, once a steady-state has been achieved, we close our
-# logging file and checkpoint solution fields to disk. These can later be used to
-# restart the simulation, if required.
-
-# +
-plog.close()
-
-with CheckpointFile("Final_State.h5", "w") as final_checkpoint:
-    final_checkpoint.save_mesh(mesh)
-    final_checkpoint.save_function(T, name="Temperature")
-    final_checkpoint.save_function(z, name="Stokes")
-    final_checkpoint.save_function(psi, name="Level set")
-# -
-
-# We can visualise the final level-set field using Firedrake's built-in plotting
-# functionality.
+# Let us finally examine the location of the material interface at the end of the
+# simulation.
 
 # + tags=["active-ipynb"]
-# import matplotlib.pyplot as plt
 # fig, axes = plt.subplots()
 # axes.set_aspect("equal")
+# contours = tricontourf(psi, levels=linspace(0.0, 1.0, 11), axes=axes, cmap="PiYG")
 # tricontour(psi, axes=axes, levels=[0.5])
+# fig.colorbar(contours, label="Conservative level-set")
 # -
