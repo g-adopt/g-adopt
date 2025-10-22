61b77466f3a02b68fd0cbbde692a383e36774575
Stephan Kramer <s.kramer@imperial.ac.uk>
2025-10-22 03:02:58 +0100

    Fix weak velocity bcs for compressible (#310)
    Co-authored-by: dsroberts <115434035+dsroberts@users.noreply.github.com>
    Co-authored-by: Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>

---
+++ tests/unit/test_symmetry.py
@@ -0,0 +1,98 @@
+import firedrake as fd
+import gadopt
+import pytest
+
+N = 4  # resolution in all directions
+mesh1d = fd.UnitIntervalMesh(N)
+mesh1dcircle = fd.CircleManifoldMesh(N)
+mesh2dtri = fd.UnitSquareMesh(N, N, quadrilateral=False)
+mesh2dquad = fd.UnitSquareMesh(N, N, quadrilateral=True)
+mesh2dcs = fd.UnitCubedSphereMesh()
+mesh2dico = fd.IcosahedralSphereMesh(1)
+meshes = {
+    "2D-tri": mesh2dtri,
+    "2D-quad": mesh2dquad,
+    "2D-extruded": fd.ExtrudedMesh(mesh1d, N),
+    "3D-tet": fd.UnitCubeMesh(N, N, N, hexahedral=False),
+    "3D-hex": fd.UnitCubeMesh(N, N, N, hexahedral=True),
+    "3D-extruded": fd.ExtrudedMesh(mesh2dquad, N),
+    "3D-extruded-prism": fd.ExtrudedMesh(mesh2dtri, N),
+    "2D-cylinder": fd.ExtrudedMesh(mesh1dcircle, N),
+    "3D-cubed-sphere": fd.ExtrudedMesh(mesh2dcs, N),
+    "3D-icosahedral-sphere": fd.ExtrudedMesh(mesh2dico, N)
+}
+
+
+@pytest.fixture(scope="module", params=meshes.items(), ids=meshes.keys())
+def mesh(request):
+    id, mesh = request.param
+    mesh.cartesian = not any(x in id for x in ['cylinder', 'sphere'])
+    return mesh
+
+
+@pytest.fixture(scope="module",
+                params=[gadopt.BoussinesqApproximation,
+                        gadopt.ExtendedBoussinesqApproximation,
+                        gadopt.TruncatedAnelasticLiquidApproximation,
+                        gadopt.AnelasticLiquidApproximation])
+def approximation(request):
+    Ra = 1
+    Di = 1
+    if request.param is gadopt.BoussinesqApproximation:
+        return request.param(Ra)
+    else:
+        return request.param(Ra, Di)
+
+
+@pytest.fixture(scope="module", params=["TaylorHood",])
+def solution_space(request, mesh):
+    # at the moment only P2-P1 is supported
+    # would like to test discontinuous velocity, but
+    # that requires the pressure gradient term to handle that
+    match request.param:
+        case "TaylorHood":
+            V = fd.VectorFunctionSpace(mesh, "CG", 2)
+            W = fd.FunctionSpace(mesh, "CG", 1)
+            return V * W
+        case _:
+            raise ValueError("Unknown discretisation type")
+
+
+def test_stokes_symmetry(approximation, mesh, solution_space):
+    """Test symmetry of discretised Stokes matrix where expected
+
+    In particular, tests symmetry of weak bc terms."""
+    z = fd.Function(solution_space)
+    u, p = z.subfunctions
+    # use a velocity that's not divergence free, to test symmetry of div(u) terms:
+    X = fd.SpatialCoordinate(mesh)
+    u.interpolate(X)
+
+    T = fd.Function(solution_space.sub(1))
+    boundary = gadopt.get_boundary_ids(mesh)
+    bids = list(vars(boundary).values())
+    bcs = {bids[0]: {'un': 0}, bids[1]: {'normal_stress': 0}}
+    # cylindrical/spherical meshes only have 2 boundaries
+    # if we have more, let's test some more bc types
+    if len(bids) > 2:
+        dim = mesh.geometric_dimension()
+        zero_vec = fd.Constant([0] * dim)
+        bcs[bids[2]] = {'stress': zero_vec}
+        # note that we are only testing the weak bc terms here
+        # weak "u" is not actually supported at the moment
+        # (but will need to be for future element pairs)
+        # at the moment type "u" is convert to a strong DirichletBC()
+        bcs[bids[3]] = {'u': zero_vec}
+    solver = gadopt.StokesSolver(z, approximation, T, bcs=bcs)
+
+    if approximation.compressible:
+        # only the velocity block will be symmetric
+        M = fd.assemble(fd.derivative(solver.F, z), mat_type='nest')
+        # the velocity block is assembled as type 'baij' for which .isSymmetric()
+        # appears to not work (always returns False); so convert to type 'aij'
+        M00 = M.petscmat.getNestSubMatrix(0, 0).convert('aij')
+        assert M00.isSymmetric(1e-13)
+    else:
+        # test symmetry of entire matrix
+        M = fd.assemble(fd.derivative(solver.F, z), mat_type='aij')
+        assert M.petscmat.isSymmetric(1e-13)
