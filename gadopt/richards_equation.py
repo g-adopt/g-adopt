adab15e81ffa2465cc391d4b4d608aa24848d216
Sia Ghelichkhan <ghelichkhani.siavash@gmail.com>
2025-10-27 16:11:30 +1100

    Lagging C

--- gadopt/richards_equation.py
+++ gadopt/richards_equation.py
@@ -79,6 +79,8 @@ def richards_mass_term(
     # Evaluate nonlinear coefficients at PREVIOUS solution (explicit treatment)
     theta = soil_curve.moisture_content(solution_old)
     C = soil_curve.water_retention(solution_old)
+    # theta = soil_curve.moisture_content(trial)
+    # C = soil_curve.water_retention(trial)
 
     # Effective saturation
     S = (theta - soil_curve.theta_r) / (soil_curve.theta_s - soil_curve.theta_r)
