cf825e2d4b9cf6cdbbac7a59919888c7d3eb0087
Stephan Kramer <s.kramer@imperial.ac.uk>
2025-11-11 10:58:03 +0000

    Update demos/mantle_convection/Drucker_Prager/Drucker_Prager.py
    Co-authored-by: Rhodri Davies <rhodri.davies@anu.edu.au>

--- demos/mantle_convection/Drucker_Prager/Drucker_Prager.py
+++ demos/mantle_convection/Drucker_Prager/Drucker_Prager.py
@@ -5,7 +5,7 @@
 # (2016)](https://doi.org/10.1002/2015GC006228) with a Drucker-Prager
 # visco-plastic rheology that depends on dynamic pressure. As this leads to
 # nonlinear systems that can be challenging to solve, we use this case to
-# illustrate some of the strategies involved.
+# illustrate some of the strategies involved in doing so within G-ADOPT.
 #
 # Similar to the [visco-plastic tutorial](viscoplastic_case.md) which uses a
 # temperature and depth dependent von Mises rheology, the overall effective
