ac1142d311e8ce56d05ac7312382adef64a9ea88
w3168 <wis15@ic.ac.uk>
2025-11-17 16:57:54 +1100

    use assert_allclose

--- demos/glacial_isostatic_adjustment/adjoint_ice/test_adjoint_ice.py
+++ demos/glacial_isostatic_adjustment/adjoint_ice/test_adjoint_ice.py
@@ -1,16 +1,16 @@
 from pathlib import Path
-import math
+import numpy as np
 
 
 def test_adjoint_taylortest():
     with open(Path(__file__).parent.resolve() / "taylor_test_minconv.txt", "r") as f:
         tt_minconv = [float(x) for x in f.readlines()]
 
-    assert math.isclose(tt_minconv[-1], 2, rel_tol=2e-2)
+    np.testing.assert_allclose(tt_minconv[-1], 2, rtol=2e-2)
 
 
 def test_adjoint_optimisation():
     with open(Path(__file__).parent.resolve() / "functional.txt", "r") as f:
         functional_values = [float(x) for x in f.readlines()]
 
-    assert math.isclose(functional_values[-1], 0.000726, rel_tol=1e-2)
+    np.testing.assert_allclose(functional_values[-1], 0.000726, rtol=1e-2)
