cd7fd9529375ee80c6ab535df4bca19322d337da
Thomas Duvernay <td75013@hotmail.fr>
2025-11-17 16:20:22 +1100

    Add tutorial for level-set initialisation (#335)
    Here, I provide a tutorial on level-set initialisation, specifically on
    the use of `assign_level_set_values`. Most, if not all, scenarios are
    demonstrated, providing a reference for anyone seeking to run
    multi-material simulations and feeling unsure about level-set
    initialisation.
    
    ---------
    
    Co-authored-by: Dale Roberts <dale.roberts1@anu.edu.au>

--- gadopt/level_set_tools.py
+++ gadopt/level_set_tools.py
@@ -124,7 +124,7 @@ def assign_level_set_values(
     | tuple[tuple[float, float], float]
     | None = None,
     interface_callable: Callable | str | None = None,
-    interface_args: tuple[Any, ...] | None = None,
+    interface_args: tuple[Any, ...] = (),
     boundary_coordinates: list[tuple[float, float]] | None = None,
 ) -> None:
     """Updates level-set field given interface thickness and signed-distance function.
@@ -153,9 +153,10 @@ def assign_level_set_values(
       form `(x, y) = (x(t), y(t))`. In this case, `interface_geometry` should be `curve`
       and `interface_callable` must be provided along with any `interface_args` to
       implement the parametric equation. `interface_callable` can be a user-defined
-      `Callable` or a `str` matching an implemented preset. Additionally,
-      `boundary_coordinates` must be supplied as a `list` of coordinates along domain
-      boundaries to enclose the 1-side of the conservative level set.
+      `Callable` or a `str` matching an implemented preset. Additionally, if the curve
+      is not closed, `boundary_coordinates` must be supplied as a `list` of coordinates
+      along domain boundaries to ensure the desired material gets attributed the 1-side
+      of the conservative level set.
     - The material interface is a polygonal chain, and `interface_geometry` takes the
       value `polygon`. If the polygonal chain is closed, either `interface_coordinates`
       or `interface_callable` must be provided. The former is a `list` of vertex
@@ -285,11 +286,6 @@ def assign_level_set_values(
         raise ValueError(
             "'interface_geometry' must be 'shapely' when providing 'interface'"
         )
-    if interface_callable is not None and interface_geometry == "circle":
-        raise ValueError(
-            "'interface_callable' must not be provided when 'interface_geometry' is "
-            "'circle'"
-        )
 
     callable_presets = {"cosine": cosine, "line": line, "rectangle": rectangle}
     if isinstance(interface_callable, str):
@@ -305,18 +301,27 @@ def assign_level_set_values(
 
     match interface_geometry:
         case "curve":
-            if boundary_coordinates is None:
-                raise ValueError(
-                    "'boundary_coordinates' must be provided when 'interface_geometry' "
-                    "is 'curve'"
-                )
-
             interface = sl.LineString(interface_coordinates)
-            enclosed_side = sl.Polygon(
-                np.vstack((interface_coordinates, boundary_coordinates))
-            )
 
-            signed_distance = sgn_dist_open_itf(interface, enclosed_side, level_set)
+            if interface.is_closed:
+                if boundary_coordinates is not None:
+                    raise ValueError(
+                        "'boundary_coordinates' must be omitted when the provided curve "
+                        "is closed"
+                    )
+
+                signed_distance = sgn_dist_closed_itf(sl.Polygon(interface), level_set)
+            else:
+                if boundary_coordinates is None:
+                    raise ValueError(
+                        "'boundary_coordinates' must be supplied when the provided curve "
+                        "is open"
+                    )
+
+                enclosed_side = sl.Polygon(
+                    np.vstack((interface_coordinates, boundary_coordinates))
+                )
+                signed_distance = sgn_dist_open_itf(interface, enclosed_side, level_set)
         case "polygon":
             if boundary_coordinates is None:
                 interface = sl.Polygon(interface_coordinates)
@@ -330,6 +335,17 @@ def assign_level_set_values(
 
                 signed_distance = sgn_dist_open_itf(interface, enclosed_side, level_set)
         case "circle":
+            if interface_callable is not None:
+                raise ValueError(
+                    "'interface_callable' must not be provided when "
+                    "'interface_geometry' is 'circle'"
+                )
+            if boundary_coordinates is not None:
+                raise ValueError(
+                    "'boundary_coordinates' must not be provided when "
+                    "'interface_geometry' is 'circle'"
+                )
+
             centre, radius = interface_coordinates
             interface = sl.Point(centre).buffer(radius)
 
