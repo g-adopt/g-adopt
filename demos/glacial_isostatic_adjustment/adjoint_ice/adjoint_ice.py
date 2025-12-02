6e1ae5ee68fb64cd73b10535e5cea06c1d87cdb1
w3168 <wis15@ic.ac.uk>
2025-12-02 17:33:15 +1100

    move lvv setup to gadopt demo utils module

--- demos/glacial_isostatic_adjustment/adjoint_ice/adjoint_ice.py
+++ demos/glacial_isostatic_adjustment/adjoint_ice/adjoint_ice.py
@@ -91,9 +91,11 @@ from gadopt.inverse import *
 
 from gadopt.utility import (
     CombinedSurfaceMeasure,
-    initialise_background_field,
-    vertical_component)
-from gadopt_demo_utils.gia_demo_utils import ice_sheet_disc
+    initialise_background_field)
+from gadopt_demo_utils.gia_demo_utils import (
+    ice_sheet_disc,
+    setup_heterogenous_viscosity
+)
 
 # + tags=["active-ipynb"]
 # from gadopt_demo_utils.gia_demo_utils import (
@@ -145,7 +147,7 @@ m = Function(S, name="internal variable")
 # -
 
 # Let's set up the background profiles for the material properties with the same values
-# as before.
+# as before, as well as the laterally varying viscosity field.
 
 # +
 X = SpatialCoordinate(mesh)
@@ -185,116 +187,7 @@ background_viscosity = Function(DG1, name="background viscosity")
 initialise_background_field(
     background_viscosity, viscosity_values_nondim, X, radius_values_nondim)
 
-
-def bivariate_gaussian(x, y, mu_x, mu_y, sigma_x, sigma_y, rho, normalised_area=False):
-    arg = ((x-mu_x)/sigma_x)**2 - 2*rho*((x-mu_x)/sigma_x)*((y-mu_y)/sigma_y) + ((y-mu_y)/sigma_y)**2
-    numerator = exp(-1/(2*(1-rho**2))*arg)
-    if normalised_area:
-        denominator = 2*pi*sigma_x*sigma_y*(1-rho**2)**0.5
-    else:
-        denominator = 1
-    return numerator / denominator
-
-
-def setup_heterogenous_viscosity(
-        background_viscosity: Function,
-        viscosity_scale: float = 1e21,
-        r_lith: float = 6301e3,
-        domain_depth: float = 2891e3,
-) -> Function:
-    '''Adds lateral variations to a background viscosity field in a 2D annulus
-
-    The synthetic lateral viscosity variations consist of 5 'blobs'
-    constructed from bivariate gaussian functions to represent interesting features
-    in the mantle. We assume the background viscosity only varies in the
-    radial direction and do not make modifications to the viscosity structure
-    in the lithosphere i.e. for r > `r_lith' where r is the radial distance.
-
-    Args:
-      background_viscosity:
-        Background radial viscosity field (N.b. this is not modified)
-      viscosity_scale:
-        Characteristc viscosity used for nondimensionalisation
-      r_lith:
-        Radius of the lithosphere-mantle boundary in m
-      domain_depth:
-        Domain depth in m used for nondimensionalisation
-
-    Returns:
-      heterogenous_viscosity_field
-        A new field containing the updated lateral viscosity variations
-    '''
-    heterogenous_viscosity_field = Function(background_viscosity.function_space(),
-                                            name='viscosity')
-
-    # Set up magnitudes of low and high viscosity regions
-    low_visc = 1e20/viscosity_scale
-    high_visc = 1e22/viscosity_scale
-
-    # Add a low viscosity region in the bottom left corner
-    # of the domain, aiming to mimic the low viscosity zone under the West
-    # Antarctic ice sheet
-    southpole_x, southpole_y = -2e6/domain_depth, -5.5e6/domain_depth
-    low_viscosity_southpole = bivariate_gaussian(X[0], X[1],
-                                                 southpole_x, southpole_y,
-                                                 1.5e6/domain_depth,
-                                                 0.5e6/domain_depth,
-                                                 -0.4)
-
-    heterogenous_viscosity_field.interpolate(
-        low_visc*low_viscosity_southpole + background_viscosity * (1-low_viscosity_southpole))
-
-    # Add two symmetrical low viscosity zones near the core-mantle boundary, inspired by
-    # Large Low-Shear-Velocity Provinces (referred to as `llsvp`) so that
-    # we can investigate sensitivity in the lower mantle.
-    llsvp1_x, llsvp1_y = 3.5e6/domain_depth, 0
-    llsvp1 = bivariate_gaussian(X[0], X[1], llsvp1_x, llsvp1_y, 0.75e6/domain_depth,
-                                1e6/domain_depth, 0)
-
-    heterogenous_viscosity_field.interpolate(low_visc*llsvp1 +
-                                             heterogenous_viscosity_field * (1-llsvp1))
-
-    llsvp2_x, llsvp2_y = -3.5e6/domain_depth, 0
-    llsvp2 = bivariate_gaussian(X[0], X[1], llsvp2_x, llsvp2_y, 0.75e6/domain_depth,
-                                1e6/domain_depth, 0)
-
-    heterogenous_viscosity_field.interpolate(low_visc*llsvp2 +
-                                             heterogenous_viscosity_field * (1-llsvp2))
-
-    # Add an elongated high viscosity region in the top right corner of the domain
-    # to represent a slab geometry
-    slab_x, slab_y = 3e6/domain_depth, 4.5e6/domain_depth
-    slab = bivariate_gaussian(X[0], X[1], slab_x, slab_y, 0.7e6/domain_depth,
-                              0.35e6/domain_depth, 0.7)
-
-    heterogenous_viscosity_field.interpolate(high_visc*slab +
-                                             heterogenous_viscosity_field * (1-slab))
-
-    # Add a high viscosity feature at the top of the domain representing a craton
-    high_viscosity_craton_x, high_viscosity_craton_y = 0, 6.2e6/domain_depth
-    high_viscosity_craton = bivariate_gaussian(X[0], X[1], high_viscosity_craton_x,
-                                               high_viscosity_craton_y,
-                                               1.5e6/domain_depth,
-                                               0.5e6/domain_depth, 0.2)
-
-    heterogenous_viscosity_field.interpolate(
-        high_visc*high_viscosity_craton +
-        heterogenous_viscosity_field * (1-high_viscosity_craton)
-    )
-
-    # We usually assume the lithosphere is purely elastic for GIA simulations,
-    # so we reset viscosity in the lithosphere to the original background viscosity
-    # value, which is assumed to be an arbitarily high constant so that the Maxwell
-    # time in this layer is much larger than the timestep and simulation duration.
-    heterogenous_viscosity_field.interpolate(
-        conditional(vertical_component(X) > r_lith/domain_depth,
-                    background_viscosity,
-                    heterogenous_viscosity_field))
-
-    return heterogenous_viscosity_field
-
-
-viscosity = setup_heterogenous_viscosity(background_viscosity)
+viscosity = setup_heterogenous_viscosity(X, background_viscosity)
 # -
 
 # Defining the Control
