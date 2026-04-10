c4c016fd789804141b9fb17904d035b4e9af467e
Liam Morrow <52473947+MorrowL@users.noreply.github.com>
2026-04-10 16:20:06 +1000

    Add files via upload

--- demos/groundwater/three_dimensions/cockett.py
+++ demos/groundwater/three_dimensions/cockett.py
@@ -6,32 +6,39 @@ Three-dimensional infultration of water into a heterogeneous soil
 Here we present an example of the infultration into a heterogeneous column of soil composed of a mixture of sand and loamy sand, as described by
     Cockett, Heagy, and Haber, Computers and Geosciences, 2018, Efficient 3D inversions using the Richards equation
     https://doi.org/10.1016/j.cageo.2018.04.006
-Simulations are formed in a rectangular prism of side length 2.0 x 2.0 x 2.6 m. No flux is imposed on all the boundaries except the top where $h = -0.1$ m.
+Simulations are formed in a rectangular prism of side length 2.0 x 2.0 x 2.6 m. No flux is imposed on all the boundaries except the top and bottom where $h = -0.1$ and -0.3 m, respectively.
 """
 
-# Set up mesh
-nodesX = 50
-nodesY, nodesZ = nodesX, round(1.3*nodesX)
-Lx, Ly, Lz = 2, 2, 2.6
 
-mesh2D = RectangleMesh(nodesX, nodesY, Lx, Ly, quadrilateral=True)
-mesh   = ExtrudedMesh(mesh2D, nodesZ, layer_height=Lz/nodesZ)
+Lx, Ly, Lz = 2, 2, 2.6              # Domain dimensions [m]
+refinement_levels = 0               # Number of refinement levels of mesh hierarchy for GMG
+factor = 1.26**0                    # Weak-scaling factor (kept at 1.0 here)
+nodes = 128/(2**refinement_levels)  # nodes in x/y direction for coarse mesh
+nodes_x, nodes_y, nodes_z = round(factor*nodes), round(factor*nodes), round(factor*1.3*nodes)
+
+# --- Mesh construction ---
+base_mesh = RectangleMesh(nodes_x, nodes_y, Lx, Ly, quadrilateral=True)  # Create a 2D quadrilateral base mesh
+mh2d = MeshHierarchy(base_mesh, refinement_levels=refinement_levels)     # Build a hierarchy of refined 2D meshes
+layer_list = [nodes_z * (2**i) for i in range(refinement_levels + 1)]    # Number of vertical layers at each refinement level
+mh3d = ExtrudedMeshHierarchy(mh2d, height=Lz, layers=layer_list)         # Extrude the 2D hierarchy into 3D
+mesh = mh3d[-1]
 
 X = SpatialCoordinate(mesh)
 
-dt = Constant(600.0)  # Time step size
-t_final = 259200 # 72 hours in seconds
+dt = Constant(300.0)  # Time step size
+t_final = 259200      # 72 hours in seconds
 
 V = FunctionSpace(mesh, "DQ", 0)
+PETSc.Sys.Print("The number of degrees of freedom is:", V.dim())
 
-# Construct the heterogeneuous soil
-epsilon = 1/500
-r = [0.0729, 0.0885, 0.7984, 0.9430, 0.6837, 0.1321, 0.7227, 0.1104, 0.1175, 0.6407]
+# --- Construction of heterogeneous soil properties ---
+epsilon = 1/500  # Controls sharpness of material transitions
+r = [0.0729, 0.0885, 0.7984, 0.9430, 0.6837, 0.1321, 0.7227, 0.1104, 0.1175, 0.6407]  # Random phase shifts for the sinusoidal modes (fixed for reproducibility
 I = sin(3*(X[0]-r[0])) + sin(3*(X[1]-r[1])) + sin(3*(X[2]-r[2])) + sin(3*(X[0]-r[3])) + sin(3*(X[1]-r[4])) + sin(3*(X[2]-r[5]))+sin(3*(X[0]-r[6])) + sin(3*(X[1]-r[7])) + sin(3*(X[2]-r[8]))
-I = 0.5*(1 + tanh(I/epsilon))
+I = 0.5*(1 + tanh(I/epsilon))  # Smoothly map to [0, 1]
 
 # Specify the hydrological parameters
-soil_curves = VanGenuchtenCurve(
+soil_curve = VanGenuchtenCurve(
     theta_r=0.02*I + 0.035*(1-I),    # Residual water content [-]
     theta_s=0.417*I + 0.401*(1-I),   # Saturated water content [-]
     Ks=5.82e-05*I + 1.69e-05*(1-I),  # Saturated hydraulic conductivity [m/s]
@@ -40,7 +47,7 @@ soil_curves = VanGenuchtenCurve(
     Ss=0,                            # Specific storage coefficient [1/m]
 )
 
-# Set up boundary conditions
+# -- Set up boundary conditions --
 boundary_ids = get_boundary_ids(mesh)
 top_bc, bottom_bc = -0.1, -0.3
 richards_bcs = {
@@ -53,18 +60,18 @@ richards_bcs = {
 }
 
 # Initial condition
-moisture_content = soil_curves.moisture_content
+moisture_content = soil_curve.moisture_content
 h     = Function(V, name="PressureHead").interpolate(bottom_bc - (bottom_bc-top_bc)*exp(5*(X[2]-Lz)))
-h_old = Function(V, name="PreviousSolution").assign(h)
 theta = Function(V, name='MoistureContent').interpolate(moisture_content(h))
 
 time_var = Constant(0)
-eq = RichardsEquation(V=V,
-                    soil_curves=soil_curves,
-                    bcs=richards_bcs,
-                    time_integrator='ImplicitMidpoint',
-                    )
-richards_solver = RichardsSolver(h, h_old, time_var, dt, eq)
+richards_solver = RichardsSolver(
+    h,
+    soil_curve,
+    delta_t=dt,
+    timestepper=BackwardEuler,
+    bcs=richards_bcs,
+)
 
 output = VTKFile("cockett.pvd")
 output.write(h, theta, time=0)
@@ -74,7 +81,6 @@ plot_iteration = 0
 
 while time < t_final:
 
-    h_old.assign(h)
     time_var.assign(time)
     richards_solver.solve()
     time += float(dt)
@@ -83,3 +89,14 @@ while time < t_final:
     if plot_iteration % 10 == 0:
         theta.interpolate(moisture_content(h))
         output.write(h, theta, time=time)
+
+    snes = richards_solver.ts.stepper.solver.snes
+    nl_it, l_it = snes.getIterationNumber(), 0
+    for i in range(snes.getIterationNumber()):
+        l_it += snes.getKSP().getIterationNumber()
+
+    PETSc.Sys.Print(f"Timestep number: {plot_iteration} | " 
+        f"t = {time/(3600):.2f} h | "
+        f"NL iters = {nl_it} | "
+        f"L iters = {l_it} | "
+        )
---
+++ demos/groundwater/three_dimensions/tracy.py
@@ -0,0 +1,100 @@
+from gadopt import *
+
+"""
+Comparison to Tracy's three dimensional exact solution
+====================================================
+Here we compare numerical solutions with the exact solution derived in
+    Tracy, 2006, Water Resources Research, Clean two- and three-dimensional solutions of Richards' equation for testing numerical solvers
+    https://doi.org/10.1029/2005WR004638
+The simulation is performed on a cube of side length L = 15.24 metres. Dirichlet boundaries conditions are imposed on the bottom, left and right boundaries $h = -L$. For the top boundary, we have
+    $$h(x,z=L,t) = (1/alpha)*ln(exp(alpha*h_r) + h_0*(sin(pi*x/L)))$$
+where $\alpha=0.25$, $hr=-L$, and $h_0 =  1 - exp(alpha*h_r)$. For the initial condition, we use $h$ from Tracy's exact solution at $t=2000$. We compute the L2 norm of h_{numerical}-h_{exact}
+
+"""
+
+L, nodes    = 15.24, 51
+
+dt, t_final = Constant(5e04), 5e06  # Steady state solution
+#dt, t_final = Constant(1e03), 1e05 # Transient solution
+
+polynomial_degree = 1
+time_integrator   = BackwardEuler
+
+soil_curve = ExponentialCurve(
+    theta_r=0.15,  # Residual water content [-]
+    theta_s=0.45,  # Saturated water content [-]
+    Ks=1.00e-05,   # Saturated hydraulic conductivity [m/s]
+    alpha=0.25,    # Fitting parameter [1/m]
+    Ss=0.00,       # Specific storage coefficient [1/m]
+)
+
+alpha = soil_curve.parameters["alpha"]
+hr = -L
+h0 = 1 - exp(alpha*hr)
+
+
+def exact_solution(x, t):
+
+    # Exact solution from Tracy 2006
+    h0 = 1 - exp(alpha * hr)
+    beta = sqrt(alpha**2/4 + (pi/L)**2 + (pi/L)**2)
+    hss = h0*sin(pi*X[0]/L)*sin(pi*X[1]/L)*exp((alpha/2)*(L - X[2]))*sinh(beta*X[2])/sinh(beta*L)
+    c = alpha*(soil_curve.parameters["theta_s"] - soil_curve.parameters["theta_r"])/soil_curve.parameters["Ks"]
+
+    phi = 0
+    for k in range(1, 200):
+        lambdak = k*pi/L
+        gamma = (beta**2 + lambdak**2)/c
+        phi = phi + ((-1)**k)*(lambdak/gamma)*sin(lambdak*X[2])*exp(-gamma*t)
+    phi = phi*((2*h0)/(L*c))*sin(pi*X[0]/L)*sin(pi*X[1]/L)*exp(alpha*(L-X[2])/2)
+
+    hBar = hss + phi
+    hExact = ((1/alpha)*ln(exp(alpha*hr) + hBar))
+
+    return hExact
+
+mesh2D = RectangleMesh(nodes, nodes, L, L, quadrilateral=True)
+mesh   = ExtrudedMesh(mesh2D, nodes, layer_height=L/nodes)
+V      = FunctionSpace(mesh, "DQ", polynomial_degree)
+X      = SpatialCoordinate(mesh)
+
+offset = 2000
+h = Function(V, name="InitialCondition").interpolate(exact_solution(X, offset))
+hExact = exact_solution(X, t_final+offset)
+
+# Boundary conditions
+boundary_ids = get_boundary_ids(mesh)
+top_bc = (1/alpha)*ln(exp(alpha*hr) + (h0)*(sin(pi*X[0]/L)*sin(pi*X[1]/L)))
+richards_bcs = {
+    boundary_ids.left:   {'h': -L},
+    boundary_ids.right:  {'h': -L},
+    boundary_ids.back:   {'h': -L},
+    boundary_ids.front:  {'h': -L},
+    boundary_ids.bottom: {'h': -L},
+    boundary_ids.top:    {'h': top_bc},
+}
+
+richards_solver = RichardsSolver(
+    h,
+    soil_curve,
+    delta_t=dt,
+    timestepper=time_integrator,
+    bcs=richards_bcs,
+)
+
+time = 0
+while time < t_final:
+
+    richards_solver.solve()
+    time += float(dt)
+
+    # Do adaptive timestepping for steady state
+    if t_final > 1e05:
+        snes = richards_solver.ts.stepper.solver.snes
+        nl_it = snes.getIterationNumber()
+        if nl_it < 3:
+            dt.assign(dt*1.05)
+
+# Compute L2 norm of error
+dx_quad = dx(metadata={"quadrature_degree": 3})
+PETSc.Sys.Print("L2 error: ", sqrt(assemble((h - hExact)**2 * dx_quad)))
