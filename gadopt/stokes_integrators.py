dc6f6b639fe00dbad2e9108a9bb30572ecbbcd0e
w3168 <wis15@ic.ac.uk>
2026-09-04 20:53:58 +1000

    actually fix test

--- gadopt/stokes_integrators.py
+++ gadopt/stokes_integrators.py
@@ -1233,8 +1233,7 @@ class CoupledInternalVariableSolver(StokesSolverBase):
         # for SIPG terms in the viscosity term of momentum_equation.py
         # N.b. the potential for confusion as GIA modellers often use
         # mu to represent the shear modulus.
-        #approximation.mu = approximation.mu0 #effective_viscosity(dt)
-        approximation.mu = approximation.effective_viscosity(dt)
+        approximation.mu = approximation.mu0
         self.scaling_factor = scaling_factor
 
         super().__init__(solution, approximation, dt=dt, theta=self._theta, **kwargs)
