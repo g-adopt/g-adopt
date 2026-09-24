8abc75e59b8f9c61112cf166cb18c0e99a4b9b61
w3168 <william.scott1@anu.edu.au>
2026-09-24 11:07:02 +1000

    remove B_mu from post seismic term

--- gadopt/momentum_equation.py
+++ gadopt/momentum_equation.py
@@ -200,9 +200,7 @@ def stress_glut_term(
 ) -> Form:
 
     if eq.stress_glut is not None:
-
-        B_mu = eq.approximation.B_mu
-        F = B_mu*inner(grad(eq.test), eq.stress_glut) * eq.dx
+        F = inner(grad(eq.test), eq.stress_glut) * eq.dx
     else:
         F = 0
 
